5d1f51e5b2bf2adb862eb2291f9edc033ec2bab0
Ahmed Eleliemy <ahmed.eleliemy@unibas.ch>
2021-05-04 11:51:24 +0200

    supporting replace to new copy or in-place updates

--- src/runtime/local/kernels/Replace.h
+++ src/runtime/local/kernels/Replace.h
@@ -20,24 +20,25 @@
 #include <runtime/local/datastructures/CSRMatrix.h>
 #include <runtime/local/datastructures/DenseMatrix.h>
 
+#include <string.h>
 #include <cstddef>
 
 // ****************************************************************************
 // Struct for partial template specialization
 // ****************************************************************************
 
-template<class DTRes, typename VT>
+template<class DTRes, class DTArg, typename VT>
 struct Replace {
-    static void apply(DTRes *& res, VT pattern, VT replacement) = delete;
+	static void apply(DTRes *& res, DTArg *& arg, VT pattern, VT replacement) = delete;
 };
 
 
 // ****************************************************************************
 // Convenience function
 // ****************************************************************************
-template<class DTRes, typename VT>
-void replace(DTRes *& res, VT pattern, VT replacement ) {
-    Replace<DTRes, VT>::apply(res, pattern, replacement);
+template<class DTRes, class DTArg, typename VT>
+void replace(DTRes *& res, DTArg *& arg, VT pattern, VT replacement ) {
+	Replace<DTRes, DTArg, VT>::apply(res, arg, pattern, replacement);
 }
 
 // ****************************************************************************
@@ -46,41 +47,70 @@ void replace(DTRes *& res, VT pattern, VT replacement ) {
 
 // ----------------------------------------------------------------------------
 // DenseMatrix <- DenseMatrix
-// 
+//
 template<typename VT>
-struct Replace<DenseMatrix<VT>, VT> {
-	static void apply(DenseMatrix<VT> *& res, VT pattern, VT replacement) {
+struct Replace<DenseMatrix<VT>, DenseMatrix<VT>, VT> {
+	static void apply(DenseMatrix<VT> *& res, DenseMatrix<VT> *& arg, VT pattern, VT replacement) {
 		//------handling corner cases -------
-		if(res==nullptr){//one might throw an exception - nullptr 
+		if(arg==nullptr){//one might throw an exception - nullptr
 			return;
 		}
-	        const size_t numRows = res->getNumRows();
-        	const size_t numCols = res->getNumCols();
-		const size_t rowSkipRes = res->getRowSkip();
-		if(numRows==0 && numRows==numCols){// one might throw an exception - empty matrix 
-			return;	
+		const size_t numRows = arg->getNumRows(); // number of rows
+		const size_t numCols = arg->getNumCols(); // number of columns
+		const size_t rowSkipRes = arg->getRowSkip(); // number of row skips
+		DenseMatrix<VT> *  targetMatrix = arg; // this will point to the matrix that should be updated. Default is arg "in-place update"
+		bool requireCopy=false; // this variable is to indicate whether we need to copy to res (when not using inplace update semantic)
+		if(numRows==0 && numRows==numCols){// one might throw an exception - empty matrix
+			return;
+		}
+		if(res!=arg && res!=nullptr){ //one might throw an exception -- res should be either pointing to the same location of arg or equals to a nullptr
+			return;
 		}
 		if((replacement!=replacement && pattern!=pattern) || (pattern == replacement)){// nothing to be done pattern equals replacement
-			return;
+			if(res!=nullptr){// do nothing
+			}
+			else{
+				res = DataObjectFactory::create<DenseMatrix<VT>>(numRows, numCols,  false);
+				//copy and return in this case replace will be a copy function that copies arg to res
+				memcpy(res, arg, numRows*numRows*sizeof(VT));
+				return;
+			}
 		}
-		//--------main logic -------------------------- 
+
+		if(res==nullptr){
+			res = DataObjectFactory::create<DenseMatrix<VT>>(numRows, numCols, false);
+			requireCopy=true;
+			targetMatrix=res;
+		}
+
+		//--------main logic --------------------------
 		if(pattern!=pattern){ // pattern is NaN
 			for(size_t r=0;r<numRows;r++){
-				VT * valuesOfRow = res->getValues() + (r*rowSkipRes);
+				VT * valuesOfRow = arg->getValues() + (r*rowSkipRes);
+				VT * updatedValuesOfRow = targetMatrix->getValues() + (r*rowSkipRes);
 				for (size_t c=0; c<numCols;c++){
-					if(valuesOfRow[c]!=valuesOfRow[c])
-						valuesOfRow[c]=replacement;
+					if(valuesOfRow[c]!=valuesOfRow[c]){
+						updatedValuesOfRow[c]=replacement;
+					}
+					else if(requireCopy){
+						updatedValuesOfRow[c]=valuesOfRow[c];
+					}
 				}
 			}
 		}
 		else{ // pattern is not NaN --> replacement can still be NaN
-			 for(size_t r=0;r<numRows;r++){
-                                VT * valuesOfRow = res->getValues() + (r*rowSkipRes);
-                                for (size_t c=0; c<numCols;c++){
-                                        if(valuesOfRow[c]==pattern)
-                                                valuesOfRow[c]=replacement;
-                                }
-                        }	
+			for(size_t r=0;r<numRows;r++){
+				VT * valuesOfRow = arg->getValues() + (r*rowSkipRes);
+				VT * updatedValuesOfRow = targetMatrix->getValues() + (r*rowSkipRes);
+				for (size_t c=0; c<numCols;c++){
+					if(valuesOfRow[c]==pattern){
+						updatedValuesOfRow[c]=replacement;
+					}
+					else if(requireCopy){
+						updatedValuesOfRow[c]=valuesOfRow[c];
+					}
+				}
+			}
 		}
 	}
 };
@@ -90,9 +120,9 @@ struct Replace<DenseMatrix<VT>, VT> {
 // ----------------------------------------------------------------------------
 
 template<typename VT>
-struct Replace<CSRMatrix<VT>, VT> {
-        static void apply(CSRMatrix<VT> *& res, VT pattern, VT replacement) {
-	//TODO 
+struct Replace<CSRMatrix<VT>, CSRMatrix<VT>, VT> {
+	static void apply(CSRMatrix<VT> *& res, CSRMatrix<VT> *& arg, VT pattern, VT replacement) {
+		//TODO
 	}
 };
 
