2320f2dcc995d43b8fecf403c91bfdaa553d0015
Patrick Damme <pdamme@know-center.at>
2021-06-02 17:35:36 +0200

    Minor changes.
    - Consistent parameter types.
    - Formatting and indentation.
    - Simplification regarding copying.

--- test/runtime/local/kernels/ReplaceTest.cpp
+++ test/runtime/local/kernels/ReplaceTest.cpp
@@ -105,7 +105,7 @@ TEMPLATE_PRODUCT_TEST_CASE("Replace", TAG_KERNELS, (DATA_TYPES), (VALUE_TYPES)){
 }
 
 TEMPLATE_PRODUCT_TEST_CASE("Replace-nan", TAG_KERNELS, (DATA_TYPES), (VALUE_TYPES_SPECIAL_CASE)){
-     using DT = TestType;
+    using DT = TestType;
     //inplace updates
 
     auto initMatrix = genGivenVals<DT>(4, {
