30491ef0d16c80c3ff21fcd793672130429619b3
Patrick Damme <pdamme@know-center.at>
2021-06-08 18:33:07 +0000

    Initial foundation of a script-level test framework and first script-level tests.
    - General foundation for script-level tests.
      - Utility functions for running programs (e.g. the command line interface of the DAPHNE Prototype), capturing and comparing their output and status codes.
      - The script-level tests are triggered by catch2 test cases, which guarantees a seamless integration with our existing test workflow.
      - Building the tests triggers building daphnec, i.e., when the tests build successfully, the prototype itself also builds successfully.
    - First script-level tests for control flow and scoping in DaphneDSL.
      - Each test case consists of one DaphneDSL script file (.daphne).
      - Some test cases only check for the expected status code (especially when a parser error is expected).
      - Other test cases check for the expected output, whereby the reference is taken from a plain text file (.txt).
      - Classical catch2 test cases determine what to do with those script files.
    - Closes #19.

---
+++ test/api/cli/scoping/ScopingTest.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 The DAPHNE Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <api/cli/StatusCode.h>
+#include <api/cli/Utils.h>
+
+#include <tags.h>
+
+#include <catch.hpp>
+
+#include <string>
+
+const std::string dirPath = "test/api/cli/scoping/";
+
+TEST_CASE("block, success", TAG_SCOPING) {
+    for(unsigned i = 1; i <= 3; i++) {
+        DYNAMIC_SECTION("block_success_" << i << ".daphne") {
+            compareDaphneToRef(dirPath, "block_success", i);
+        }
+    }
+}
+
+#define MAKE_FAILURE_TEST_CASE(name, count) \
+    TEST_CASE(name ", failure", TAG_SCOPING) { \
+        for(unsigned i = 1; i <= count; i++) { \
+            DYNAMIC_SECTION(name "_failure_" << i << ".daphne") { \
+                checkDaphneStatusCode(dirPath, name, i, StatusCode::PARSER_ERROR); \
+            } \
+        } \
+    }
+
+MAKE_FAILURE_TEST_CASE("block", 2)
+MAKE_FAILURE_TEST_CASE("if", 3)
+MAKE_FAILURE_TEST_CASE("for", 2)
+MAKE_FAILURE_TEST_CASE("while", 2)
