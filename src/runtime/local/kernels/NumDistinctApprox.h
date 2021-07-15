be9aee944ec56a0d76b92cf5ef0dddb51e5f229e
coffeecup <dominic.schablas@student.tugraz.at>
2021-07-15 08:22:50 +0200

    Improving CSRMatrix handling of numDistinctApprox. * Now exploiting the sparsity for CSRMatrix.

--- src/runtime/local/kernels/NumDistinctApprox.h
+++ src/runtime/local/kernels/NumDistinctApprox.h
@@ -84,96 +84,28 @@ template <typename VT> struct NumDistinctApprox<CSRMatrix<VT>> {
         const size_t numRows = arg->getNumRows();
         const size_t numCols = arg->getNumCols();
         const size_t numElements = numRows * numCols;
-        const VT* values = arg->getValues();
-        const size_t* rowOffsets = arg->getRowOffsets();
-        const VT zero = 0;
 
         UniqueBoundedPriorityQueue<uint32_t> pQueue(K);
         uint32_t hashedValueOut = 0;
 
-        size_t rowIdx = 0;
-        size_t colIdx = 0;
-        size_t elementIdx = 0;
-        bool stopInit = false;
-        const size_t * colIdxBegin = arg->getColIdxs(rowIdx);
-        const size_t * colIdxEnd = arg->getColIdxs(rowIdx+1);
-
-      for(size_t rowIdx = 0; rowIdx < numRows; rowIdx++) {
-          for(size_t colIdx = 0; colIdx < numCols; colIdx++) {
-              VT el = arg->get(rowIdx, colIdx);
-              MurmurHash3_x86_32(&el, sizeof(VT), seed, &hashedValueOut);
-              pQueue.push(hashedValueOut);
-          }
-      }
-        /*
-        while( rowIdx < numRows ) {
-            colIdx = 0;
-            colIdxBegin = arg->getColIdxs(rowIdx);
-            colIdxEnd = arg->getColIdxs(rowIdx+1);
-
-            while( colIdx < numCols) {
-
-                const size_t * ptrExpected = std::lower_bound(colIdxBegin, colIdxEnd, colIdx);
-                const VT * el;
-
-                // Check wether this colIdx does exist in this row's colIdxs.
-                if(ptrExpected == colIdxEnd || *ptrExpected != colIdx) {
-                    el = &zero;
-                } else {
-                    el = (values + rowOffsets[rowIdx]) + (ptrExpected - colIdxBegin);
-                }
-
-                MurmurHash3_x86_32(el, sizeof(VT), seed, &hashedValueOut);
-                pQueue.push(hashedValueOut);
-
-                elementIdx++;
-
-                if (elementIdx >= K) {
-                    stopInit = true;
-                    break;
-                }
-                colIdx++;
-            }
-
-            if (stopInit) {
-                break;
-            }
-            rowIdx++;
+        const size_t numNonZeros = arg->getNumNonZeros();
+        if (numElements > numNonZeros) { // at least one zero.
+            const VT zero = 0;
+            MurmurHash3_x86_32(&zero, sizeof(VT), seed, &hashedValueOut);
+            pQueue.push(hashedValueOut);
         }
 
+        for(size_t rowIdx = 0; rowIdx < numRows; rowIdx++) {
+            const VT* values = arg->getValues(rowIdx);
 
-        // rowIdx/colIdx are now at the element where we left off.
-        while ( rowIdx < numRows) {
-            colIdxBegin = arg->getColIdxs(rowIdx);
-            colIdxEnd = arg->getColIdxs(rowIdx+1);
-
-            while ( colIdx < numCols) {
-
-                const size_t * ptrExpected = std::lower_bound(colIdxBegin, colIdxEnd, colIdx);
-                const VT * el;
-
-                // Check wether this colIdx does exist in this row's colIdxs.
-                if(ptrExpected == colIdxEnd || *ptrExpected != colIdx) {
-                    el = &zero;
-                } else {
-                    el = (values + rowOffsets[rowIdx]) + (ptrExpected - colIdxBegin);
-                }
-
-                MurmurHash3_x86_32(el, sizeof(VT), seed, &hashedValueOut);
-
-                if (hashedValueOut < pQueue.top()) {
-                    pQueue.pop();
-                    pQueue.push(hashedValueOut);
-                }
-
-                colIdx++;
+            const size_t numNonZerosInRow = arg->getNumNonZeros(rowIdx);
+            for(size_t colIdx = 0; colIdx < numNonZerosInRow; colIdx++) {
+                VT el = values[colIdx];
+                MurmurHash3_x86_32(&el, sizeof(VT), seed, &hashedValueOut);
+                pQueue.push(hashedValueOut);
             }
+        }
 
-            colIdx = 0;
-            rowIdx++;
-       }
-
-       */
       size_t kMinVal = pQueue.top();
       const size_t maxVal = std::numeric_limits<std::uint32_t>::max();
       double kMinValNormed =
