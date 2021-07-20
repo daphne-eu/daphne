7a684bdcdba9efd708d526d25854330871383804
Patrick Damme <pdamme@know-center.at>
2021-07-20 16:42:44 +0200

    More test cases for numDistinctApprox-kernel.
    - More diverse cases for the input data (not only unique values).
    - Revealing issues with the kernel (tests fail at the moment).

--- test/runtime/local/kernels/NumDistinctApproxTest.cpp
+++ test/runtime/local/kernels/NumDistinctApproxTest.cpp
@@ -21,6 +21,7 @@
 #include <runtime/local/datastructures/CSRMatrix.h>
 #include <runtime/local/datastructures/DenseMatrix.h>
 #include <runtime/local/kernels/NumDistinctApprox.h>
+#include <runtime/local/kernels/RandMatrix.h>
 #include <stdexcept>
 #include <tags.h>
 #include <catch.hpp>
@@ -28,23 +29,45 @@
 TEMPLATE_PRODUCT_TEST_CASE("numDistinctApprox", TAG_KERNELS, (DenseMatrix, CSRMatrix), (double, uint32_t)) {
 
     using DT = TestType;
+    using VT = typename DT::VT;
 
+#if 0
+    // The old data generation.
     const size_t numElements = 10000;
     std::vector<typename DT::VT> v(numElements,0);
     std::srand(123456789);
 
     std::generate_n(v.begin(), numElements/100, std::rand);
-    auto mat10000 = genGivenVals<DT>(100, v);
-
-    SECTION("numDistinctApprox distinct") {
-
-        // Allow +/-10% error. When error is bigger something is either
-        // wrong parametriced (K to small) or the algorithm broke.
-        auto approxResult = numDistinctApprox(mat10000, 64);
-        auto isResultBelow10PercentOff = approxResult <= 110 && approxResult >= 90;
-
-        CHECK(isResultBelow10PercentOff);
+    auto mat = genGivenVals<DT>(100, v);
+#elif 1
+    // The new data generation.
+    const size_t numRows = 100;
+    const size_t numCols = 100;
+    const double sparsity = 0.1;
+    // Each of the following cases generates approximately 1000 non-zero values.
+    // The test case is automatically run separately for each of the sections.
+    DT * mat = nullptr;
+    size_t expectedNumDistinct;
+    SECTION("unique values") {
+        randMatrix<DT, VT>(mat, numRows, numCols, VT(0), VT(100000000), sparsity, -1);
+        expectedNumDistinct = 1000;
     }
+    SECTION("many, non-unique values") { // more than k unique values
+        randMatrix<DT, VT>(mat, numRows, numCols, VT(0), VT(200), sparsity, -1);
+        expectedNumDistinct = 200;
+    }
+    SECTION("few, non-unique values") { // less than k unique values
+        randMatrix<DT, VT>(mat, numRows, numCols, VT(0), VT(10), sparsity, -1);
+        expectedNumDistinct = 10;
+    }
+#endif
+
+    // Allow +/-20% error. When error is bigger something is either
+    // wrong parametriced (K to small) or the algorithm broke.
+    auto approxResult = numDistinctApprox(mat, 64);
+    const size_t tolerance = static_cast<size_t>(expectedNumDistinct * 0.2);
+    CHECK(approxResult >= expectedNumDistinct - tolerance);
+    CHECK(approxResult <= expectedNumDistinct + tolerance);
 }
 
 TEMPLATE_PRODUCT_TEST_CASE("numDistinctApprox - Dense-Submatrix", TAG_KERNELS, (DenseMatrix), (double, uint32_t)) {
