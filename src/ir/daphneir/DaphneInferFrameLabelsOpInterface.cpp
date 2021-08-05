7807e266252c9e1ae5b113a4a636ec7c70422be9
Patrick Damme <pdamme@know-center.at>
2021-08-05 22:50:07 +0200

    Column label inference for ExtractColOp, ExtractRowOp, and ColBindOp.

--- src/ir/daphneir/DaphneInferFrameLabelsOpInterface.cpp
+++ src/ir/daphneir/DaphneInferFrameLabelsOpInterface.cpp
@@ -44,8 +44,59 @@ std::string getConstantString(Value v) {
 }
 
 // ****************************************************************************
+// Frame label inference utility functions
+// ****************************************************************************
+// For families of operations.
+
+template<class ExtractOrFilterRowOp>
+void inferFrameLabels_ExtractOrFilterRowOp(ExtractOrFilterRowOp * op) {
+    Type t = op->source().getType();
+    if(auto ft = t.dyn_cast<daphne::FrameType>()) {
+        Value res = op->getResult();
+        auto oldResType = res.getType().dyn_cast<daphne::FrameType>();
+        auto newResType = daphne::FrameType::get(
+                op->getContext(),
+                oldResType.getColumnTypes(),
+                ft.getLabels()
+        );
+        res.setType(newResType);
+    }
+}
+
+// ****************************************************************************
 // Frame label inference implementations
 // ****************************************************************************
+
+void daphne::ColBindOp::inferFrameLabels() {
+    auto ftLhs = lhs().getType().dyn_cast<daphne::FrameType>();
+    auto ftRhs = rhs().getType().dyn_cast<daphne::FrameType>();
+    
+    if(!ftLhs || !ftRhs)
+        throw std::runtime_error(
+                "currently ColBindOp can only infer its output labels if both "
+                "inputs are frames"
+        );
+    if(!ftLhs.getLabels() || !ftRhs.getLabels())
+        throw std::runtime_error(
+                "currenly ColBindOp can only infer its output labels if the "
+                "labels of both input frames are known"
+        );
+    
+    auto labelsRes = new std::vector<std::string>();
+    for(auto l : *(ftLhs.getLabels()))
+        labelsRes->push_back(l);
+    for(auto l : *(ftRhs.getLabels()))
+        labelsRes->push_back(l);
+    
+    Value res = getResult();
+    auto oldResType = res.getType().dyn_cast<daphne::FrameType>();
+    auto newResType = daphne::FrameType::get(
+            getContext(),
+            oldResType.getColumnTypes(),
+            labelsRes
+    );
+    res.setType(newResType);
+}
 
 void daphne::CreateFrameOp::inferFrameLabels() {
     auto resLabels = new std::vector<std::string>();
@@ -61,17 +112,35 @@ void daphne::CreateFrameOp::inferFrameLabels() {
     res.setType(newResType);
 }
 
-void daphne::FilterRowOp::inferFrameLabels() {
-    if(auto ft = source().getType().dyn_cast<daphne::FrameType>()) {
+void daphne::ExtractColOp::inferFrameLabels() {
+    auto ft = source().getType().dyn_cast<daphne::FrameType>();
+    auto st = selectedCols().getType().dyn_cast<daphne::StringType>();
+    if(ft && st) {
+        auto resLabels = new std::vector<std::string>();
+        resLabels->push_back(getConstantString(selectedCols()));
+        
         Value res = getResult();
         auto oldResType = res.getType().dyn_cast<daphne::FrameType>();
         auto newResType = daphne::FrameType::get(
                 getContext(),
                 oldResType.getColumnTypes(),
-                ft.getLabels()
+                resLabels
         );
         res.setType(newResType);
     }
+    else
+        throw std::runtime_error(
+                "currently ExtractColOp can only infer its output labels for "
+                "frame inputs and a single column name"
+        );
+}
+
+void daphne::ExtractRowOp::inferFrameLabels() {
+    inferFrameLabels_ExtractOrFilterRowOp(this);
+}
+
+void daphne::FilterRowOp::inferFrameLabels() {
+    inferFrameLabels_ExtractOrFilterRowOp(this);
 }
 
 void daphne::GroupJoinOp::inferFrameLabels() {
