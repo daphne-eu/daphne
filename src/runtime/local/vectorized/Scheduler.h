2e52daa1936446fe400c481a38d5b11eb46a398c
Ahmed Eleliemy <ahmed.eleliemy@unibas.ch>
2021-09-12 10:11:24 +0200

    restructure the scheduler class- seperate chunk calculation from task assignment - prepare scheduler to be used with compiled pipe line tasks

--- src/runtime/local/vectorized/Scheduler.h
+++ src/runtime/local/vectorized/Scheduler.h
@@ -17,111 +17,71 @@
 #ifndef SRC_RUNTIME_LOCAL_VECTORIZED_SCHEDULER_H
 #define SRC_RUNTIME_LOCAL_VECTORIZED_SCHEDULER_H
 
-#include <runtime/local/vectorized/TaskQueues.h>
-#include <runtime/local/vectorized/Tasks.h>
-#include <runtime/local/vectorized/Workers.h>
-#include <ir/daphneir/Daphne.h>
-
-#include <thread>
-#include <functional>
-
-template <class VT>
 class Scheduler {
 
 private:
-    std::string mode;
-    uint64_t rlen;
-    uint32_t n_proc;
-    DenseMatrix<VT>* res;
-    DenseMatrix<VT>* input1;
-    DenseMatrix<VT>* input2;
-    uint64_t initChunkSize = 1;
-
-
+    int schedulingMethod;
+    uint64_t totalTasks;
+    uint64_t chunkParam;
+    uint64_t scheduledTasks;
+    uint64_t remainingTasks;
+    uint32_t totalWorkers;
+    uint64_t schedulingStep;
+    uint64_t tssChunk; 
+    uint64_t tssDelta;
 public:
-    Scheduler() : Scheduler(){};
-    Scheduler(uint64_t rlen, uint32_t n_proc, DenseMatrix<VT>*& res, DenseMatrix<VT>* input1, DenseMatrix<VT>* input2, std::string mode){
-        this.rlen = rlen;
-        this.n_proc = n_proc;
-        this.res = res;
-        this.input1 = input1;
-        this.input2 = input2;
-        this.mode = mode;
+    Scheduler(int method, uint64_t tasks, uint64_t chunk, uint32_t workers){ 
+        schedulingMethod = method;
+        totalTasks = tasks;
+        chunkParam = chunk;
+        totalWorkers = workers;
+        remainingTasks = tasks;
+        schedulingStep = 0;
+        scheduledTasks = 0;
+        tssChunk = (uint64_t) ceil((double) totalTasks / ((double) 2*totalWorkers));
+        uint64_t nTemp = (uint64_t) ceil(2*totalTasks/(tssChunk+1));
+        tssDelta  = (uint64_t) (tssChunk - 1)/(double)(nTemp-1);
     }
+    bool hasNextChunk(){
+        return scheduledTasks < totalTasks; 
+    }  
+    uint64_t getNextChunk(){
+        uint64_t chunkSize = 0;
+        switch (schedulingMethod){
+            case 0:{//STATIC
+                    chunkSize = (uint64_t)ceil(totalTasks/totalWorkers);
+                    break;
+            }
+            case 1:{// SS
+                    chunkSize = 1;
+                    break;
+            }
+            case 2:{//GSS
+                    chunkSize = (uint64_t)ceil((double)remainingTasks/totalTasks);
+                    break;
+            }
+            case 3:{//TSS
+                    chunkSize = tssChunk - tssDelta * schedulingStep;
+                    break;
+            }
+            case 4:{//FAC2
+                    uint64_t actualStep = schedulingStep/totalWorkers; // has to be an integer division 
+                    chunkSize = (uint64_t) ceil(pow(0.5,actualStep+1)*(totalTasks/totalWorkers));
+                    break;
+            }
 
-    void initChunk(uint32_t initChunk){
-        this.initChunkSize = initChunk;
+            default:{
+                    chunkSize = (uint64_t)ceil(totalTasks/totalWorkers/4.0);
+                    break;
+            }
     }
-
-
-    void run(void (*func)(DenseMatrix<VT>*,DenseMatrix<VT>*,DenseMatrix<VT>*)){
-        TaskQueue* q = new BlockingTaskQueue(this.input1->getNumRows());
-        uint64_t chunkSize = 1;
-        uint64_t batchsize = 1;
-        uint64_t remaining = 0;
-        uint32_t k = 0;
-        switch(this.mode){
-            case "SCH_STATIC":
-                //STATIC
-                chunkSize = (uint64_t)ceil((double)rlen/n_proc/4);
-                batchsize = 1; // row-at-a-time
-                for(k=0; (k<n_proc*4) & (k*chunkSize<rlen); k++) {
-                    q->enqueueTask(new SingleOpTask<VT>(
-                        func, this.res, this.input1, this.input2, k*chunkSize, std::min((k+1)*chunkSize,rlen), batchsize));
-                }
-                q->closeInput();
-                break;
-            case "SCH_GSS":
-                //GSS
-                remaining = rlen - (uint64_t)ceil((double)rlen/n_proc);
-                chunkSize = 0;
-               
-                while (remaining >= n_proc){
-                    //std::cout <<  chunkSize << " " <<remaining <<"\n";
-                    chunkSize = (uint64_t)ceil((double)remaining/n_proc);
-                    rlen = remaining;
-                    remaining = rlen - (uint64_t)ceil((double)rlen/n_proc);
-                    q->enqueueTask(new SingleOpTask<VT>(
-                        func, this.res, this.input1, this.input2, k*chunkSize, std::min((k+1)*chunkSize,rlen), batchsize));
-                
-                    k++;
-                }
-                q->closeInput();
-                break;
-
-            case "SCH_TFSS":
-                        //TFSS
-                n_proc = 4;
-                remaining = rlen - (uint64_t)ceil((double)rlen/n_proc);
-                chunkSize = (uint64_t)ceil((double)rlen/n_proc);
-
-                rlen = 100;
-                chunkSize = 0;
-                double steps  = ceil(2.0*rlen/(chunkSize+1)); //n=2N/f+l
-                double tss_delta = (double) (chunkSize - 1)/(double) (steps-1);
-            
-                while(remaining >= n_proc){
-                    //std::cout <<  chunkSize << " " <<remaining <<"\n";
-                    chunkSize = ceil((double) rlen / ((double) 2*n_proc)); 
-                    rlen = remaining;
-                    remaining = rlen - tss_delta;
-                    q->enqueueTask(new SingleOpTask<VT>(
-                        func, this.res, this.input1, this.input2, k*chunkSize, std::min((k+1)*chunkSize,rlen), batchsize));
-                    k++;
-                }
-                q->closeInput();
-                break;
-            case "SCH_FAC2":
-
-                n_proc = 4;
-                //dbl_parm1 = (double)rlen / (double)n_proc;
-
-                //pr->u.p.parm1 = chunk;
-                //pr->u.p.dbl_parm1 = dbl_parm1;
-                break;
-        
-        }
-    }
+    chunkSize = std::max(chunkSize,chunkParam);
+    chunkSize = std::min(chunkSize, remainingTasks);
+    schedulingStep++;
+    scheduledTasks+=chunkSize;
+    remainingTasks-=chunkSize;
+    return chunkSize;
+    }  
 
 };
 
