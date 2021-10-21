212d8306998ec0bac2c0b7d357f36809cd5eae7e
Izajasz Wrosz <izajasz.p.wrosz@intel.com>
2021-10-21 15:57:34 +0000

    [DAPHNE-#150] Quantize operation and CPU reference implementation.
    - DaphneDSL built-in: dst = quantize(src, min, max);
      where src and dst are DenseMatrices of float and uint8_t elements respectively.
    - A corresponding DaphneIR operation and a reference kernel CPU implementation.
    - Adds a test case for Quantization.
    - Closes #150.

---
+++ test/runtime/local/kernels/QuantizeTest.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 The DAPHNE Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <runtime/local/kernels/Quantize.h>
+#include <runtime/local/datagen/GenGivenVals.h>
+
+#include <tags.h>
+#include <catch.hpp>
+#include <string>
+#include <vector>
+#include <cstdint>
+
+TEST_CASE("Quantization", TAG_KERNELS) {
+
+    auto f0 = genGivenVals<DenseMatrix<float>>(2, {
+        0, 1.0,
+	0.5, 1.1});
+
+    DenseMatrix<uint8_t>* res = nullptr;
+
+    quantize(res, f0, 0, 1, nullptr);
+
+    CHECK(res->getNumRows() == 2);
+    CHECK(res->getNumCols() == 2);
+
+    CHECK(res->get(0,0) == 0);
+    CHECK(res->get(0,1) == 255);
+    CHECK(res->get(1,0) == 128);
+    CHECK(res->get(1,1) == 255);
+}
