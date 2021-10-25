88aa0e7f49f5726cc66d7c9d3053aacb22ca70b3
Dzevad Coralic <dzevad.coralic@student.tugraz.at>
2021-10-25 16:03:04 +0200

    added lincenses

--- src/api/python/script_building/dag.py
+++ src/api/python/script_building/dag.py
@@ -1,3 +1,23 @@
+# -------------------------------------------------------------
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# -------------------------------------------------------------
 from abc import ABC
 from enum import Enum, auto
 from typing import TYPE_CHECKING, Any, Dict, Sequence, Union, Optional
@@ -47,9 +67,6 @@ class DAGNode(ABC):
     def script(self):
         return self._script
 
-    @property
-    def script_str(self):
-        return self._script.dml_script
 
     @property
     def dsl_name(self):
--- test/api/python/test_1.py
+++ test/api/python/test_1.py
@@ -1,4 +1,23 @@
-import os
+# -------------------------------------------------------------
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# -------------------------------------------------------------
 import unittest
 import numpy as np
 from api.python.operator.nodes.matrix import Matrix
@@ -8,7 +27,7 @@ import random
 np.random.seed(7)
 # TODO Remove the randomness of the test, such that
 #      inputs for the random operation is predictable
-shape = (random.randrange(1, 25), random.randrange(1, 25))
+shape = (random.randrange(1, 7), random.randrange(1, 7))
 dist_shape = (10, 15)
 min_max = (0, 1)
 sparsity = random.uniform(0.0, 1.0)
