8763abcecec222d976a3265d754be12843b1c238
Patrick Damme <pdamme@know-center.at>
2021-11-17 00:59:15 +0100

    Script-level test cases for a few example algorithms.
    - Connected components, k-means clustering, and linear regression model training.
    - While still being toy examples, these algorithms allow us to test the combined usage of several features of the system.
    - Using small randomly generated data sets.
    - At the moment, we only check if these DaphneDSL scripts compile and execute without errors.
    - Later, we should also check the results.

---
+++ test/api/cli/algorithms/AlgorithmsTest.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021 The DAPHNE Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <api/cli/Utils.h>
+
+#include <tags.h>
+
+#include <catch.hpp>
+
+#include <sstream>
+#include <string>
+
+const std::string dirPath = "test/api/cli/algorithms/";
+
+// For now, we just check if the algorithms terminate successfully, but we
+// don't check the results.
+
+TEST_CASE("components", TAG_ALGORITHMS) {
+    checkDaphneStatusCode(
+            StatusCode::SUCCESS, dirPath + "components.daphne",
+            "--args", "n=100", "e=1000"
+    );
+}
+
+TEST_CASE("kmeans", TAG_ALGORITHMS) {
+    checkDaphneStatusCode(
+            StatusCode::SUCCESS, dirPath + "kmeans.daphne",
+            "--args", "r=100", "c=5", "f=20", "i=10"
+    );
+}
+
+TEST_CASE("lm", TAG_ALGORITHMS) {
+    checkDaphneStatusCode(
+            StatusCode::SUCCESS, dirPath + "lm.daphne",
+            "--args", "r=100", "c=20"
+    );
+}
