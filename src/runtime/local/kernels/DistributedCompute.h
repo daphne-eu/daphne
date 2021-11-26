cf20239b6ab87d68bba36ad8ff62279528455fa1
avontz@cslab.ece.ntua.gr <avontz@cslab.ece.ntua.gr>
2021-11-26 22:17:17 +0200

    DistributedCompute 1 operand support

--- src/runtime/local/kernels/DistributedCompute.h
+++ src/runtime/local/kernels/DistributedCompute.h
@@ -63,83 +63,118 @@ struct DistributedCompute<DenseMatrix<double>>
                       const char *mlirCode,
                       DCTX(ctx))
     {
-        assert(num_args == 2 && "Only binary supported for now");
-        auto lhs = args[0];
-        auto rhs = args[1];
-
-        const size_t numRowsLhs = lhs->getRows();
-        const size_t numColsLhs = lhs->getCols();
-        const size_t numRowsRhs = rhs->getRows();
-        const size_t numColsRhs = rhs->getCols();
-
+        assert((num_args == 1 || num_args == 2)&& "Only binary and unary supported for now");
         struct StoredInfo {
-            DistributedIndex *ix;
-            DistributedData *data;
-        };
+                DistributedIndex *ix;
+                DistributedData *data;
+            };
         DistributedCaller<StoredInfo, distributed::Task, distributed::ComputeResult> caller;
-        Handle<DenseMatrix<double>>::HandleMap resMap;
-        if(numRowsLhs == numRowsRhs && numColsLhs == numColsRhs) {
-            for (auto &pair : lhs->getMap()) {
+        Handle<DenseMatrix<double>>::HandleMap resMap;        
+        size_t resultRows, resultColumns;
+        // ****************************************************************************
+        // Unary operations
+        // ****************************************************************************
+        if (num_args == 1){
+            auto arg = args[0];
+            resultRows = arg->getRows();
+            resultColumns = 1;
+            for (auto &pair : arg->getMap()){
                 auto ix = pair.first;
-                auto lhsData = pair.second;
-                auto rhsData = rhs->getMap().find(ix)->second;
+                auto argData = pair.second;
+                distributed::Task task;
+                *task.add_inputs()->mutable_stored() = argData.getData();
+                task.set_mlir_code(mlirCode);
+                
+                StoredInfo storedInfo ({new DistributedIndex(ix), new DistributedData(argData)});
 
-                if (lhsData.getAddress() == rhsData.getAddress()) {                
-
-                    distributed::Task task;
-                    *task.add_inputs()->mutable_stored() = lhsData.getData();
-                    *task.add_inputs()->mutable_stored() = rhsData.getData();
-                    task.set_mlir_code(mlirCode);
-                    
-                    StoredInfo storedInfo ({new DistributedIndex(ix), new DistributedData(lhsData)});
-
-                    caller.addAsyncCall(lhsData.getChannel(), storedInfo, task);
-                }
-                else {
-                    // TODO: send data between workers
-                    throw std::runtime_error(
-                        "Data shuffling not yet supported"
-                    );
-                }
+                caller.addAsyncCall(argData.getChannel(), storedInfo, task);
             }
         }
-        else if(numRowsRhs == 1 && numColsLhs == numColsRhs) {
-            for (auto &pair : lhs->getMap()) {
-                auto ix = pair.first;
-                auto lhsData = pair.second;
-                DistributedData *rhsDataPtr = nullptr;
-                for (auto &pairRhs : rhs->getMap()){
-                    if (pairRhs.second.getAddress() == lhsData.getAddress()) {
-                        rhsDataPtr = new DistributedData(pairRhs.second);
-                        break;
+        // ****************************************************************************
+        // Binary operations
+        // ****************************************************************************
+        if (num_args == 2){
+            auto lhs = args[0];
+            auto rhs = args[1];
+
+            const size_t numRowsLhs = lhs->getRows();
+            const size_t numColsLhs = lhs->getCols();
+            const size_t numRowsRhs = rhs->getRows();
+            const size_t numColsRhs = rhs->getCols();
+
+            // ****************************************************************************
+            // Handle <- Matrix @ Matrix
+            // ****************************************************************************
+            if(numRowsLhs == numRowsRhs && numColsLhs == numColsRhs) {
+                resultRows = numRowsLhs;
+                resultColumns = numColsLhs;
+                for (auto &pair : lhs->getMap()) {
+                    auto ix = pair.first;
+                    auto lhsData = pair.second;
+                    auto rhsData = rhs->getMap().find(ix)->second;
+
+                    if (lhsData.getAddress() == rhsData.getAddress()) {                
+
+                        distributed::Task task;
+                        *task.add_inputs()->mutable_stored() = lhsData.getData();
+                        *task.add_inputs()->mutable_stored() = rhsData.getData();
+                        task.set_mlir_code(mlirCode);
+                        
+                        StoredInfo storedInfo ({new DistributedIndex(ix), new DistributedData(lhsData)});
+
+                        caller.addAsyncCall(lhsData.getChannel(), storedInfo, task);
+                    }
+                    else {
+                        // TODO: send data between workers
+                        throw std::runtime_error(
+                            "Data shuffling not yet supported"
+                        );
                     }
                 }
-                auto rhsData = DistributedData(*rhsDataPtr);
-                if (lhsData.getAddress() == rhsData.getAddress()) {                
+            }
+            // ****************************************************************************
+            // Handle <- Matrix @ row
+            // ****************************************************************************
+            else if(numRowsRhs == 1 && numColsLhs == numColsRhs) {
+                resultRows = numRowsLhs;
+                resultColumns = numColsLhs;
+                for (auto &pair : lhs->getMap()) {
+                    auto ix = pair.first;
+                    auto lhsData = pair.second;
+                    DistributedData *rhsDataPtr = nullptr;
+                    for (auto &pairRhs : rhs->getMap()){
+                        if (pairRhs.second.getAddress() == lhsData.getAddress()) {
+                            rhsDataPtr = new DistributedData(pairRhs.second);
+                            break;
+                        }
+                    }
+                    auto rhsData = DistributedData(*rhsDataPtr);
+                    if (lhsData.getAddress() == rhsData.getAddress()) {                
 
-                    distributed::Task task;
-                    *task.add_inputs()->mutable_stored() = lhsData.getData();
-                    *task.add_inputs()->mutable_stored() = rhsData.getData();
-                    task.set_mlir_code(mlirCode);
+                        distributed::Task task;
+                        *task.add_inputs()->mutable_stored() = lhsData.getData();
+                        *task.add_inputs()->mutable_stored() = rhsData.getData();
+                        task.set_mlir_code(mlirCode);
                     
-                    StoredInfo storedInfo ({new DistributedIndex(ix), new DistributedData(lhsData)});
+                        StoredInfo storedInfo ({new DistributedIndex(ix), new DistributedData(lhsData)});
 
-                    caller.addAsyncCall(lhsData.getChannel(), storedInfo, task);
-                }
-                else {
-                    // TODO: send data between workers
-                    throw std::runtime_error(
-                        "Data shuffling not yet supported"
-                    );
+                        caller.addAsyncCall(lhsData.getChannel(), storedInfo, task);
+                    }
+                    else {
+                        // TODO: send data between workers
+                        throw std::runtime_error(
+                            "Data shuffling not yet supported"
+                        );
+                    }
                 }
             }
-        }
-        else {
-            assert(
-                    false && "lhs and rhs must either have the same dimensions, "
-                    "or rhs be a row/column vector with the "
-                    "width/height of the other"
-            );
+            else {
+                assert(
+                        false && "lhs and rhs must either have the same dimensions, "
+                        "or rhs be a row/column vector with the "
+                        "width/height of the other"
+                );
+            }
         }
         // Get Results
         while (!caller.isQueueEmpty()){
@@ -151,7 +186,7 @@ struct DistributedCompute<DenseMatrix<double>>
             DistributedData data(computeResult.outputs(0).stored(), lhsdata->getAddress(), lhsdata->getChannel());
             resMap.insert({*ix, data});
         }
-        res = new Handle<DenseMatrix<double>>(resMap, lhs->getRows(), lhs->getCols());
+        res = new Handle<DenseMatrix<double>>(resMap, resultRows, resultColumns);        
     }
 };
 
