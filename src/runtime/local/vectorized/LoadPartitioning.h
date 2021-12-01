b34ad124f4e97ae428f8a3ea5fbb3eed1575e563
Ahmed  Eleliemy <ahmed.eleliemy@unibas.ch>
2021-12-01 21:36:01 +0000

    [DAPHNE-#115] Self-scheduling schemes.
    - Restructure the scheduler class- separate chunk calculation from task.
    - New LoadPartitioning class used by MTWrapper.
    - Co-authored by Ahmed Eleliemy and Gabrielle Poerwawinata
    - Closes #115.

---
+++ src/runtime/local/vectorized/LoadPartitioning.h
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2021 The DAPHNE Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_RUNTIME_LOCAL_VECTORIZED_LOADPARTITIONING_H
+#define SRC_RUNTIME_LOCAL_VECTORIZED_LOADPARTITIONING_H
+
+#include <cmath>
+#include <cstdlib>
+#include <string>
+
+enum SelfSchedulingScheme { STATIC=0, SS, GSS, TSS, FAC2, TFSS, FISS, VISS, 
+                            PLS, MSTATIC, MFSC, PSS};
+class LoadPartitioning {
+
+private:
+    int schedulingMethod;
+    uint64_t totalTasks;
+    uint64_t chunkParam;
+    uint64_t scheduledTasks;
+    uint64_t remainingTasks;
+    uint32_t totalWorkers;
+    uint64_t schedulingStep;
+    uint64_t tssChunk; 
+    uint64_t tssDelta;
+    uint64_t mfscChunk;
+    uint32_t fissStages;
+    int getMethod (const char * method){
+        return std::stoi(method);
+    }
+    int getStages(int tasks, int workers){
+        int actual_step=0;
+        int scheduled=0;
+        int step=0;
+        while (scheduled < tasks){
+            int actual_step=step/workers;
+            double chunk = pow(0.5,actual_step+1)*tasks/float(workers);
+            scheduled+=ceil(chunk);
+            step+=1;
+        }
+        return actual_step+1;
+    }
+public:
+    LoadPartitioning(int method, uint64_t tasks, int64_t chunk, uint32_t workers, bool autochunk){
+        
+        if(const char* env_m = std::getenv("DAPHNE_TASK_PARTITION")){
+            method= getMethod(env_m);
+        } 
+        schedulingMethod = method;
+        totalTasks = tasks;
+        double tSize = (totalTasks+workers-1.0)/workers;
+        mfscChunk = ceil(tSize*log(2.0)/log((1.0*tSize)));
+        fissStages = getStages(totalTasks, workers);
+        if(!autochunk){    
+            chunkParam = chunk;
+        }
+        else{
+            chunkParam = mfscChunk; //indicate automatic chunk parameter
+        }
+        totalWorkers = workers;
+        remainingTasks = tasks;
+        schedulingStep = 0;
+        scheduledTasks = 0;
+        tssChunk = (uint64_t) ceil((double) totalTasks / ((double) 2.0*totalWorkers));
+        uint64_t nTemp = (uint64_t) ceil(2.0*totalTasks/(tssChunk+1.0));
+        tssDelta  = (uint64_t) (tssChunk - 1.0)/(double)(nTemp-1.0);
+    }
+    bool hasNextChunk(){
+        return scheduledTasks < totalTasks; 
+    }  
+    uint64_t getNextChunk(){
+        uint64_t chunkSize = 0;
+        switch (schedulingMethod){
+            case STATIC:{//STATIC
+                chunkSize = (uint64_t) ceil(totalTasks/totalWorkers);
+                break;
+            }
+            case SS:{// self-scheduling (SS)
+                chunkSize = 1;
+                break;
+            }
+            case GSS:{//guided self-scheduling (GSS)
+                chunkSize = (uint64_t) ceil((double)remainingTasks/totalWorkers);
+                break;
+            }
+            case TSS:{//trapezoid self-scheduling (TSS)
+                chunkSize = tssChunk - tssDelta * schedulingStep;
+                break;
+            }
+            case FAC2:{//factoring (FAC2)
+                uint64_t actualStep = schedulingStep/totalWorkers; // has to be an integer division 
+                chunkSize = (uint64_t) ceil(pow(0.5,actualStep+1)*(totalTasks/totalWorkers));
+                break;
+            }
+            case TFSS:{//trapezoid factoring self-scheduling (TFSS)
+                chunkSize = (uint64_t) ceil((double) remainingTasks/ ((double) 2.0*totalWorkers));
+                break;
+            }
+            case FISS:{//fixed increase self-scheduling (FISS)
+                //TODO
+                uint64_t X = fissStages + 2;
+                uint64_t initChunk = (uint64_t) ceil(totalTasks/((2.0+fissStages)*totalWorkers));
+                chunkSize = initChunk + schedulingStep * (uint64_t) ceil((2.0*totalTasks*(1.0-(fissStages/X)))/(totalWorkers*fissStages*(fissStages-1))); //chunksize with increment after init 
+                break;
+            }
+            case VISS:{//variable increase self-scheduling (VISS)
+                //TODO
+                uint64_t schedulingStepnew =  schedulingStep/totalWorkers;
+                uint64_t initChunk = (uint64_t) ceil(totalTasks/((2.0+fissStages)*totalWorkers));
+                chunkSize =  initChunk * (uint64_t) ceil((double)(1-pow(0.5,schedulingStepnew))/0.5);
+                break;
+            }
+            case PLS:{//performance-based loop self-scheduling (PLS)
+                //TODO
+                double SWR = 0.5; //static workload ratio
+                if(remainingTasks > totalTasks - (totalTasks*SWR)){
+                    chunkSize = (uint64_t) ceil((double)totalTasks*SWR/totalWorkers);
+                }else{
+                    chunkSize = (uint64_t) ceil((double)remainingTasks/totalWorkers);
+                }
+                break;
+            }
+            case PSS:{//probabilistic self-scheduling (PSS)
+                //E[P] is the average number of idle processor, for now we use still totalWorkers
+                double averageIdleProc = (double)totalWorkers;
+                chunkSize = (uint64_t) ceil((double)remainingTasks/(1.5*averageIdleProc));
+                //TODO
+                break;
+            }
+            case MFSC:{//modifed fixed self-scheduling (MFSC)
+                chunkSize=mfscChunk;
+                break;
+            }
+            default:{
+                chunkSize = (uint64_t)ceil(totalTasks/totalWorkers/4.0);
+                break;
+            }
+        }
+        chunkSize = std::max(chunkSize,chunkParam);
+        chunkSize = std::min(chunkSize, remainingTasks);
+        schedulingStep++;
+        scheduledTasks+=chunkSize;
+        remainingTasks-=chunkSize;
+        return chunkSize;
+    } 
+};
+
+#endif //SRC_RUNTIME_LOCAL_VECTORIZED_LOADPARTITIONING_H
