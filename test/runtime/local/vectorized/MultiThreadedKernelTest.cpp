b5015530fddaeb97429a67cbb249b883fd52edc3
Patrick Damme <pdamme@know-center.at>
2021-12-01 22:30:41 +0100

    Minor adjustments.

--- test/runtime/local/vectorized/MultiThreadedKernelTest.cpp
+++ test/runtime/local/vectorized/MultiThreadedKernelTest.cpp
@@ -47,10 +47,9 @@ TEMPLATE_PRODUCT_TEST_CASE("Multi-threaded X+Y", TAG_VECTORIZED, (DATA_TYPES), (
 
     DT *r1 = nullptr, *r2 = nullptr;
     ewBinaryMat<DT, DT, DT>(BinaryOpCode::ADD, r1, m1, m2, nullptr); //single-threaded
-    
     MTWrapper<VT>* wrapper = new MTWrapper<VT>(4);
     wrapper->execute(&funAdd, r2, m1, m2, false); //multi-threaded
-    
+
     CHECK(checkEqApprox(r1, r2, 1e-6, nullptr));
 
     delete wrapper;
@@ -70,10 +69,9 @@ TEMPLATE_PRODUCT_TEST_CASE("Multi-threaded X*Y", TAG_VECTORIZED, (DATA_TYPES), (
 
     DT *r1 = nullptr, *r2 = nullptr;
     ewBinaryMat<DT, DT, DT>(BinaryOpCode::MUL, r1, m1, m2, nullptr); //single-threaded
-    
     MTWrapper<VT>* wrapper = new MTWrapper<VT>(4);
     wrapper->execute(&funMul, r2, m1, m2, false); //multi-threaded
-    
+
     CHECK(checkEqApprox(r1, r2, 1e-6, nullptr));
 
     delete wrapper;
