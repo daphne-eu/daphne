b291c8a582802de60a1786f67584d801ef174778
Aleš Zamuda <ales@ales.uni-mb.si>
2021-12-07 10:26:29 +0100

    Added nicer colors for GnuPlot PNG terminal when drawing the timings plot.

--- deploy/experiments-CIDR-2022-Vega-microbenchmarks/store-one-deployment-output.sh
+++ deploy/experiments-CIDR-2022-Vega-microbenchmarks/store-one-deployment-output.sh
@@ -47,4 +47,5 @@ echo Logging to $logfile
 screen -L -Logfile $logfile ./Demo_on_Vega-2021-12-06-NUMCORES_1000_DAPHNE-components-42-time-N_6000_e_30-Ales_Zamuda.sh
 cp -p TIME.g ${logfile}.timing
 cp -p TIME ${logfile}.out
+cat ${logfile}.timing | gnuplot -p -e "set terminal pngcairo enhanced; set output '"${logfile}.timing.png"'; set autoscale; set xlabel 'run number: 1-5 (one node) and 6-10 (1000 DistributedWorker nodes)'; set xtics 1; set xrange [0.5:10.5]; set ylabel 'time [s]'; set key below; set style fill solid 0.8; plot '-' u (\$0+1):1 with boxes lw 2 lc rgb '#7889fb' title 'The components algorithm execution statistics.'"
 )
