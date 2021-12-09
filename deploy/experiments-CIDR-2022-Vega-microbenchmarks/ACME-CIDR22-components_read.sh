8a29682a0f992bd9f699aced0a4b1d69a93f052f
Aleš Zamuda <ales@ales.uni-mb.si>
2021-12-09 11:36:10 +0100

    Added run for components_read 10/1000 to ACME script

--- deploy/experiments-CIDR-2022-Vega-microbenchmarks/ACME-CIDR22-components_read.sh
+++ deploy/experiments-CIDR-2022-Vega-microbenchmarks/ACME-CIDR22-components_read.sh
@@ -1,6 +1,7 @@
 ./store-one-deployment-output.sh -r 10 1; 
 ./store-one-deployment-output.sh -r 10 10; 
 ./store-one-deployment-output.sh -r 10 100; 
+./store-one-deployment-output.sh -r 10 1000; 
 ./store-one-deployment-output.sh -r 100 1; 
 ./store-one-deployment-output.sh -r 100 100; 
 ./store-one-deployment-output.sh -r 100 1000; 
