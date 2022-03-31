ab549ca59b9d06ece893c3bd2536083de6ee2550
Patrick Damme <pdamme@know-center.at>
2022-03-31 20:44:02 +0200

    [MINOR] Added modification note to DaphneLib source files based on SystemDS code.

--- src/api/python/operator/nodes/scalar.py
+++ src/api/python/operator/nodes/scalar.py
@@ -17,6 +17,8 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+# Modifications Copyright 2022 The DAPHNE Consortium
+#
 # -------------------------------------------------------------
 from typing import (TYPE_CHECKING, Dict, Iterable, Optional, Sequence, Tuple,
                     Union)
--- src/api/python/utils/__init__.py
+++ src/api/python/utils/__init__.py
@@ -17,4 +17,6 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+# Modifications Copyright 2022 The DAPHNE Consortium
+#
 # -------------------------------------------------------------
