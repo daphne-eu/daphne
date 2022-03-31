797ddc56352d29aac5df215a133f437019250daf
Patrick Damme <pdamme@know-center.at>
2022-03-31 20:38:51 +0200

    Fixed license of Murmur3 hash.

--- src/util/MurmurHash3.h
+++ src/util/MurmurHash3.h
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-
+// MurmurHash3 was written by Austin Appleby, and is placed in the public
 // domain. The author hereby disclaims copyright to this source code.
 
 #pragma once
