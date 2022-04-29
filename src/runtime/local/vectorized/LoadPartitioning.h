ac62eb3a2c88874a90202dc025fa7f73427408b3
Jonathan Giger <giger.jonathan@gmail.com>
2022-04-29 11:01:48 +0200

    fix merge conflict

--- src/runtime/local/vectorized/LoadPartitioning.h
+++ src/runtime/local/vectorized/LoadPartitioning.h
@@ -22,11 +22,11 @@
 #include <string>
 
 enum QueueTypeOption { CENTRALIZED=0, PERGROUP, PERCPU};
+enum victimSelectionLogic { SEQ=0, SEQPRI, RANDOM, RANDOMPRI};
+
 enum SelfSchedulingScheme { STATIC=0, SS, GSS, TSS, FAC2, TFSS, FISS, VISS, 
                             PLS, MSTATIC, MFSC, PSS,
                             INVALID=-1 /* only for JSON enum conversion */};
-enum victimSelectionLogic { SEQ=0, SEQPRI, RANDOM, RANDOMPRI};
-
 class LoadPartitioning {
 
 private:
