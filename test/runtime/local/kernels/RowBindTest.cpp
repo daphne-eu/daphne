855a2d02696c44ac01a0144a2a5b58e7c313d10a
akroviakov <71938912+akroviakov@users.noreply.github.com>
2022-04-29 17:45:13 +0200

    [DAPHNE-#217]: rowBind-kernel for CSRMatrix (#344)
    - Implements row binding for CSRMatrix datatype.
    - Adds a new property check isView() for CSRMatrix datatype.
    - Closes #217.
    
    Co-authored-by: Artem Kroviakov <kroviakov@student.tugraz.at>

--- test/runtime/local/kernels/RowBindTest.cpp
+++ test/runtime/local/kernels/RowBindTest.cpp
@@ -16,6 +16,7 @@
 
 #include <runtime/local/datagen/GenGivenVals.h>
 #include <runtime/local/datastructures/DenseMatrix.h>
+#include <runtime/local/datastructures/CSRMatrix.h>
 #include <runtime/local/datastructures/Frame.h>
 #include <runtime/local/datastructures/Structure.h>
 #include <runtime/local/kernels/CheckEq.h>
@@ -171,4 +172,88 @@ TEMPLATE_TEST_CASE("RowBind", TAG_KERNELS, (Frame)) {
     DataObjectFactory::destroy(c26);
     DataObjectFactory::destroy(c37);
     DataObjectFactory::destroy(f0123);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE("RowBind", TAG_KERNELS, (CSRMatrix), (double, uint32_t)) {
+    using DT = TestType;
+    using VT = typename DT::VT;
+
+    size_t numRows = 4;
+    size_t numCols = 5;
+    size_t numNonZeros = 5;
+
+    auto m0 = DataObjectFactory::create<CSRMatrix<VT>>(numRows, numCols, numNonZeros, true);
+    m0->set(1,1, VT(11.));
+    m0->set(2,1, VT(21.));
+    m0->set(2,3, VT(23.));
+    m0->set(3,3, VT(33.));
+    m0->set(3,4, VT(34.));
+    
+    DT * res = nullptr;
+    
+    SECTION("Normal on normal") {
+        auto m1 = DataObjectFactory::create<CSRMatrix<VT>>(numRows, numCols, 3, true);
+        m1->set(1,0, VT(10.));
+        m1->set(1,2, VT(12.));
+        m1->set(3,0, VT(30.));
+
+        auto exp = DataObjectFactory::create<CSRMatrix<VT>>(numRows*2, numCols, 8, true);
+        exp->set(1,1, VT(11.));
+        exp->set(2,1, VT(21.));
+        exp->set(2,3, VT(23.));
+        exp->set(3,3, VT(33.));
+        exp->set(3,4, VT(34.));
+        exp->set(1 + numRows,0, VT(10.));
+        exp->set(1 + numRows,2, VT(12.));
+        exp->set(3 + numRows,0, VT(30.));
+
+        rowBind<DT, DT, DT>(res, m0, m1, nullptr);
+        CHECK(*res == *exp);
+
+        DataObjectFactory::destroy(m1);
+        DataObjectFactory::destroy(exp);
+        DataObjectFactory::destroy(res);
+    }
+
+    SECTION("Normal on view") {
+        size_t lower_bound = 1;
+        size_t upper_bound = 3;
+        auto m1 = DataObjectFactory::create<CSRMatrix<VT>>(m0, lower_bound, upper_bound);
+        auto exp = DataObjectFactory::create<CSRMatrix<VT>>(numRows + (upper_bound - lower_bound), numCols, 8, true);
+        exp->set(1,1, VT(11.));
+        exp->set(2,1, VT(21.));
+        exp->set(2,3, VT(23.));
+        exp->set(3,3, VT(33.));
+        exp->set(3,4, VT(34.));
+        exp->set(1 + (numRows-1), 1, VT(11.));
+        exp->set(2 + (numRows-1), 1, VT(21.));
+        exp->set(2 + (numRows-1), 3, VT(23.));
+
+        rowBind<DT, DT, DT>(res, m0, m1, nullptr);
+        CHECK(*res == *exp);
+
+        DataObjectFactory::destroy(m1);
+        DataObjectFactory::destroy(exp);
+        DataObjectFactory::destroy(res);
+    }
+
+    SECTION("View on view") {
+        auto m1 = DataObjectFactory::create<CSRMatrix<VT>>(m0, 2, 4);
+        auto m2 = DataObjectFactory::create<CSRMatrix<VT>>(m0, 0, 2);
+        auto exp  = DataObjectFactory::create<CSRMatrix<VT>>(numRows, numCols, numNonZeros, true);
+        exp->set(0,1, VT(21.));
+        exp->set(0,3, VT(23.));
+        exp->set(1,3, VT(33.));
+        exp->set(1,4, VT(34.));
+        exp->set(3,1, VT(11.));
+
+        rowBind<DT, DT, DT>(res, m1, m2, nullptr);
+        CHECK(*res == *exp);
+
+        DataObjectFactory::destroy(m1);
+        DataObjectFactory::destroy(m2);
+        DataObjectFactory::destroy(res);
+    }
+
+    DataObjectFactory::destroy(m0);
 }
