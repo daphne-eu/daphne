8f6cb63d3a05d97980a17ad73bcef4b631b2bc15
Artem Kroviakov <mr.kroviakov@gmail.com>
2022-05-03 11:04:57 +0200

    another test case (both row&col view)

--- test/runtime/local/kernels/ReshapeTest.cpp
+++ test/runtime/local/kernels/ReshapeTest.cpp
@@ -63,11 +63,20 @@ TEMPLATE_PRODUCT_TEST_CASE("Reshape", TAG_KERNELS, (DenseMatrix), (double, uint3
         DataObjectFactory::destroy(view);
     }
     SECTION("Reshape view 2") {
-
         const DT * initial = genGivenVals<DT>(2, vals); // 2x6
         const DT * view = DataObjectFactory::create<DT>(initial, 1, 2, 0, 6); // 1x6
         const DT * exp = genGivenVals<DT>(3, {6,7 ,8,9, 10,11}); // 3x2
         checkReshape(view, 3, 2, exp);
+
+        DataObjectFactory::destroy(exp);
+        DataObjectFactory::destroy(initial);
+        DataObjectFactory::destroy(view);
+    }
+    SECTION("Reshape view 3") {
+        const DT * initial = genGivenVals<DT>(2, vals); // 2x6
+        const DT * view = DataObjectFactory::create<DT>(initial, 1, 2, 0, 4); // 1x4
+        const DT * exp = genGivenVals<DT>(2, {6,7 ,8,9}); // 2x2
+        checkReshape(view, 2, 2, exp);
 
         DataObjectFactory::destroy(exp);
         DataObjectFactory::destroy(initial);
