7b6dd0a26e2655b5ff8d63efb50c20104f65fdde
tomschw <tom.schwarzburg@mailbox.tu-dresden.de>
2022-05-25 11:59:55 +0200

    Add select operator based on Morphstore.

--- src/runtime/local/kernels/MorphStore/select.h
+++ src/runtime/local/kernels/MorphStore/select.h
@@ -21,6 +21,14 @@
 
 #include <cstdint>
 
+#include <core/storage/column.h>
+#include "core/operators/otfly_derecompr/select.h"
+#include "core/operators/otfly_derecompr/project.h"
+#include "core/operators/otfly_derecompr/merge.h"
+#include "core/morphing/uncompr.h"
+
+#include <ir/daphneir/Daphne.h>
+
 enum class CompareOperation : uint32_t {
   Equal = 1,
   LessThan = 2,
@@ -37,14 +45,117 @@ enum class CompareOperation : uint32_t {
 template<class DTRes, class DTIn>
 class Select {
   public:
-    static void apply(DTRes * res, const DTIn * in, const char * inOn, CompareOperation cmp, uint64_t selValue) = delete;
+    static void apply(DTRes * & res, const DTIn * in, const char * inOn, CompareOperation cmp, uint64_t selValue) = delete;
 };
 
 template<class DTRes, class DTIn>
-void select(DTRes * res, const DTIn * in, const char * inOn, CompareOperation cmp, uint64_t selValue) {
+void select(DTRes * & res, const DTIn * in, const char * inOn, CompareOperation cmp, uint64_t selValue) {
     Select<DTRes, DTIn>::apply(res, in, inOn, cmp, selValue);
 }
 
+template<>
+class Select<Frame, Frame> {
+    public:
+        static void apply(Frame * & res, const Frame * in, const char * inOn, CompareOperation cmp, uint64_t selValue) {
+            auto colData = static_cast<uint64_t const *>(in->getColumnRaw(in->getColumnIdx(inOn)));
+            const morphstore::column<morphstore::uncompr_f> * const selectCol = new morphstore::column<morphstore::uncompr_f>(sizeof(uint64_t) * in->getNumRows(), colData);
 
+            using ve = vectorlib::scalar<vectorlib::v64<uint64_t> >;
 
+            const morphstore::column<morphstore::uncompr_f> * selectPos;
+
+            switch (cmp) {
+                case CompareOperation::Equal:
+
+                    selectPos = morphstore::select<
+                            ve,
+                            vectorlib::equal,
+                            morphstore::uncompr_f,
+                            morphstore::uncompr_f
+                    >(selectCol, selValue);
+                    break;
+                case CompareOperation::LessThan:
+                    selectPos = morphstore::select<
+                            ve,
+                            vectorlib::less,
+                            morphstore::uncompr_f,
+                            morphstore::uncompr_f
+                    >(selectCol, selValue);
+                    break;
+                case CompareOperation::LessEqual:
+                    selectPos = morphstore::select<
+                            ve,
+                            vectorlib::lessequal,
+                            morphstore::uncompr_f,
+                            morphstore::uncompr_f
+                    >(selectCol, selValue);
+                    break;
+                case CompareOperation::GreaterThan:
+                    selectPos = morphstore::select<
+                            ve,
+                            vectorlib::greater,
+                            morphstore::uncompr_f,
+                            morphstore::uncompr_f
+                    >(selectCol, selValue);
+                    break;
+                case CompareOperation::GreaterEqual:
+                    selectPos = morphstore::select<
+                            ve,
+                            vectorlib::greaterequal,
+                            morphstore::uncompr_f,
+                            morphstore::uncompr_f
+                    >(selectCol, selValue);
+                    break;
+                case CompareOperation::NotEqual:
+                    auto smallerPos = morphstore::select<
+                            ve,
+                            vectorlib::less,
+                            morphstore::uncompr_f,
+                            morphstore::uncompr_f
+                    >(selectCol, selValue);
+                    auto greaterPos = morphstore::select<
+                            ve,
+                            vectorlib::greater,
+                            morphstore::uncompr_f,
+                            morphstore::uncompr_f
+                    >(selectCol, selValue);
+
+                    selectPos = morphstore::merge_sorted<ve,
+                            morphstore::uncompr_f,
+                            morphstore::uncompr_f,
+                            morphstore::uncompr_f>(smallerPos, greaterPos);
+                    delete smallerPos, delete greaterPos;
+                    break;
+            }
+
+            const std::string *columnLabels = in->getLabels();
+
+            std::vector<Structure *> resultCols = {};
+
+            for (size_t i = 0; i < in->getNumCols(); ++ i) {
+                auto colProjData = static_cast<uint64_t const *>(in->getColumnRaw(in->getColumnIdx(*(columnLabels + i))));
+                auto colProj = new morphstore::column<morphstore::uncompr_f>(sizeof(uint64_t) * in->getNumRows(), colProjData);
+                auto* projCol = const_cast<morphstore::column<morphstore::uncompr_f> *>(morphstore::my_project_wit_t<ve, morphstore::uncompr_f, morphstore::uncompr_f,
+                        morphstore::uncompr_f>::apply(
+                        colProj, selectPos));
+
+                /// Change the persistence type to disable the deletion and deallocation of the data.
+                projCol->set_persistence_type(morphstore::storage_persistence_type::externalScope);
+
+                uint64_t * ptr = projCol->get_data();
+
+                std::shared_ptr<uint64_t[]> shrdPtr(ptr);
+
+                auto result = DataObjectFactory::create<DenseMatrix<uint64_t>>(projCol->get_count_values(), 1, shrdPtr);
+
+                resultCols.push_back(result);
+                delete projCol, delete colProj;
+            }
+
+            res = DataObjectFactory::create<Frame>(resultCols, columnLabels);
+
+            delete selectPos, delete selectCol;
+        }
+
+    };
 #endif //SRC_RUNTIME_LOCAL_KERNELS_MORPHSTORE_SELECT_H
--- test/runtime/local/kernels/MorphStore/SelectTest.cpp
+++ test/runtime/local/kernels/MorphStore/SelectTest.cpp
@@ -14,7 +14,8 @@
  * You should have received a copy of the GNU General Public License along with this program. *
  * If not, see <http://www.gnu.org/licenses/>.                                                *
  **********************************************************************************************/
- 
+#include <mlir/IR/Location.h>
+
 #include <tags.h>
 #include <vector>
 #include <cstdint>
@@ -27,20 +28,289 @@
 #include <runtime/local/datastructures/DataObjectFactory.h>
 #include <runtime/local/datastructures/DenseMatrix.h>
 #include <runtime/local/datastructures/Frame.h>
+#include <runtime/local/kernels/CheckEq.h>
+
 
 #include <runtime/local/kernels/MorphStore/select.h>
 
-TEST_CASE("First steps", TAG_KERNELS) {
+TEST_CASE("Morphstore Select: Test the LessThan (<) operation", TAG_KERNELS) {
     /// Data generation
     auto lhs_col0 = genGivenVals<DenseMatrix<uint64_t>>(10, { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9});
-    auto lhs_col1 = genGivenVals<DenseMatrix<int64_t>>(10,  { 0, 11, 20, 33, 44, 55, 60, 77, 88, 99});
+    auto lhs_col1 = genGivenVals<DenseMatrix<uint64_t>>(10,  { 0, 11, 20, 33, 44, 55, 60, 77, 88, 99});
     std::vector<Structure *> lhsCols = {lhs_col0, lhs_col1};
     std::string lhsLabels[] = {"R.idx", "R.a"};
     auto f = DataObjectFactory::create<Frame>(lhsCols, lhsLabels);
-    
-    Frame * result = nullptr;
-    
-    select(result, f, "R.a", CompareOperation::GreaterThan, 50);
-    
-    /// check results ...
+
+    size_t selectValue = 55;
+
+    Frame * expectedResult;
+    /// create expected result set
+    {
+        std::vector<uint64_t> er_col0_val;
+        std::vector<uint64_t> er_col1_val;
+        for (uint64_t i = 0; i < lhs_col0->getNumRows(); ++ i) {
+            /// condition to check
+            if (lhs_col1->get(i, 0) < selectValue) {
+                er_col0_val.push_back(lhs_col0->get(i, 0));
+                er_col1_val.push_back(lhs_col1->get(i, 0));
+            }
+        }
+        uint64_t size = er_col0_val.size();
+        auto er_col0 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col0_val);
+        auto er_col1 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col1_val);
+        /// create result data
+        expectedResult = DataObjectFactory::create<Frame>(
+                std::vector<Structure*>{er_col0,er_col1},
+                lhsLabels);
+        /// cleanup
+        DataObjectFactory::destroy(er_col0, er_col1);
+        DataObjectFactory::destroy(lhs_col0, lhs_col1);
+    }
+
+    /// test execution
+    Frame * resultFrame = nullptr;
+
+    select(resultFrame, f, "R.a", CompareOperation::LessThan, selectValue);
+
+    /// test if result matches expected result
+    CHECK(*resultFrame == *expectedResult);
+
+    /// cleanup
+    DataObjectFactory::destroy(resultFrame, expectedResult, f);
+
+}
+
+TEST_CASE("Morphstore Select: Test the LessEqual (<=) operation", TAG_KERNELS) {
+    /// Data generation
+    auto lhs_col0 = genGivenVals<DenseMatrix<uint64_t>>(10, { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9});
+    auto lhs_col1 = genGivenVals<DenseMatrix<uint64_t>>(10,  { 0, 11, 20, 33, 44, 55, 60, 77, 88, 99});
+    std::vector<Structure *> lhsCols = {lhs_col0, lhs_col1};
+    std::string lhsLabels[] = {"R.idx", "R.a"};
+    auto f = DataObjectFactory::create<Frame>(lhsCols, lhsLabels);
+
+    size_t selectValue = 55;
+
+    Frame * expectedResult;
+    /// create expected result set
+    {
+        std::vector<uint64_t> er_col0_val;
+        std::vector<uint64_t> er_col1_val;
+        for (uint64_t i = 0; i < lhs_col0->getNumRows(); ++ i) {
+            /// condition to check
+            if (lhs_col1->get(i, 0) <= selectValue) {
+                er_col0_val.push_back(lhs_col0->get(i, 0));
+                er_col1_val.push_back(lhs_col1->get(i, 0));
+            }
+        }
+        uint64_t size = er_col0_val.size();
+        auto er_col0 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col0_val);
+        auto er_col1 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col1_val);
+        /// create result data
+        expectedResult = DataObjectFactory::create<Frame>(
+                std::vector<Structure*>{er_col0,er_col1},
+                lhsLabels);
+        /// cleanup
+        DataObjectFactory::destroy(er_col0, er_col1);
+        DataObjectFactory::destroy(lhs_col0, lhs_col1);
+    }
+
+    /// test execution
+    Frame * resultFrame = nullptr;
+
+    select(resultFrame, f, "R.a", CompareOperation::LessEqual, selectValue);
+
+    /// test if result matches expected result
+    CHECK(*resultFrame == *expectedResult);
+
+    /// cleanup
+    DataObjectFactory::destroy(resultFrame, expectedResult, f);
+
+}
+
+TEST_CASE("Morphstore Select: Test the GreaterThan (>) operation", TAG_KERNELS) {
+    /// Data generation
+    auto lhs_col0 = genGivenVals<DenseMatrix<uint64_t>>(10, { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9});
+    auto lhs_col1 = genGivenVals<DenseMatrix<uint64_t>>(10,  { 0, 11, 20, 33, 44, 55, 60, 77, 88, 99});
+    std::vector<Structure *> lhsCols = {lhs_col0, lhs_col1};
+    std::string lhsLabels[] = {"R.idx", "R.a"};
+    auto f = DataObjectFactory::create<Frame>(lhsCols, lhsLabels);
+
+    size_t selectValue = 55;
+
+    Frame * expectedResult;
+    /// create expected result set
+    {
+        std::vector<uint64_t> er_col0_val;
+        std::vector<uint64_t> er_col1_val;
+        for (uint64_t i = 0; i < lhs_col0->getNumRows(); ++ i) {
+            /// condition to check
+            if (lhs_col1->get(i, 0) > selectValue) {
+                er_col0_val.push_back(lhs_col0->get(i, 0));
+                er_col1_val.push_back(lhs_col1->get(i, 0));
+            }
+        }
+        uint64_t size = er_col0_val.size();
+        auto er_col0 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col0_val);
+        auto er_col1 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col1_val);
+        /// create result data
+        expectedResult = DataObjectFactory::create<Frame>(
+                std::vector<Structure*>{er_col0,er_col1},
+                lhsLabels);
+        /// cleanup
+        DataObjectFactory::destroy(er_col0, er_col1);
+        DataObjectFactory::destroy(lhs_col0, lhs_col1);
+    }
+
+    /// test execution
+    Frame * resultFrame = nullptr;
+
+    select(resultFrame, f, "R.a", CompareOperation::GreaterThan, selectValue);
+
+    /// test if result matches expected result
+    CHECK(*resultFrame == *expectedResult);
+
+    /// cleanup
+    DataObjectFactory::destroy(resultFrame, expectedResult, f);
+
+}
+
+TEST_CASE("Morphstore Select: Test the GreaterEqual (>=) operation", TAG_KERNELS) {
+    /// Data generation
+    auto lhs_col0 = genGivenVals<DenseMatrix<uint64_t>>(10, { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9});
+    auto lhs_col1 = genGivenVals<DenseMatrix<uint64_t>>(10,  { 0, 11, 20, 33, 44, 55, 60, 77, 88, 99});
+    std::vector<Structure *> lhsCols = {lhs_col0, lhs_col1};
+    std::string lhsLabels[] = {"R.idx", "R.a"};
+    auto f = DataObjectFactory::create<Frame>(lhsCols, lhsLabels);
+
+    size_t selectValue = 55;
+
+    Frame * expectedResult;
+    /// create expected result set
+    {
+        std::vector<uint64_t> er_col0_val;
+        std::vector<uint64_t> er_col1_val;
+        for (uint64_t i = 0; i < lhs_col0->getNumRows(); ++ i) {
+            /// condition to check
+            if (lhs_col1->get(i, 0) >= selectValue) {
+                er_col0_val.push_back(lhs_col0->get(i, 0));
+                er_col1_val.push_back(lhs_col1->get(i, 0));
+            }
+        }
+        uint64_t size = er_col0_val.size();
+        auto er_col0 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col0_val);
+        auto er_col1 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col1_val);
+        /// create result data
+        expectedResult = DataObjectFactory::create<Frame>(
+                std::vector<Structure*>{er_col0,er_col1},
+                lhsLabels);
+        /// cleanup
+        DataObjectFactory::destroy(er_col0, er_col1);
+        DataObjectFactory::destroy(lhs_col0, lhs_col1);
+    }
+
+    /// test execution
+    Frame * resultFrame = nullptr;
+
+    select(resultFrame, f, "R.a", CompareOperation::GreaterEqual, selectValue);
+
+    /// test if result matches expected result
+    CHECK(*resultFrame == *expectedResult);
+
+    /// cleanup
+    DataObjectFactory::destroy(resultFrame, expectedResult, f);
+
+}
+
+TEST_CASE("Morphstore Select: Test the Equal (==) operation", TAG_KERNELS) {
+    /// Data generation
+    auto lhs_col0 = genGivenVals<DenseMatrix<uint64_t>>(10, { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9});
+    auto lhs_col1 = genGivenVals<DenseMatrix<uint64_t>>(10,  { 0, 11, 20, 33, 44, 55, 60, 77, 88, 99});
+    std::vector<Structure *> lhsCols = {lhs_col0, lhs_col1};
+    std::string lhsLabels[] = {"R.idx", "R.a"};
+    auto f = DataObjectFactory::create<Frame>(lhsCols, lhsLabels);
+
+    size_t selectValue = 55;
+
+    Frame * expectedResult;
+    /// create expected result set
+    {
+        std::vector<uint64_t> er_col0_val;
+        std::vector<uint64_t> er_col1_val;
+        for (uint64_t i = 0; i < lhs_col0->getNumRows(); ++ i) {
+            /// condition to check
+            if (lhs_col1->get(i, 0) == selectValue) {
+                er_col0_val.push_back(lhs_col0->get(i, 0));
+                er_col1_val.push_back(lhs_col1->get(i, 0));
+            }
+        }
+        uint64_t size = er_col0_val.size();
+        auto er_col0 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col0_val);
+        auto er_col1 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col1_val);
+        /// create result data
+        expectedResult = DataObjectFactory::create<Frame>(
+                std::vector<Structure*>{er_col0,er_col1},
+                lhsLabels);
+        /// cleanup
+        DataObjectFactory::destroy(er_col0, er_col1);
+        DataObjectFactory::destroy(lhs_col0, lhs_col1);
+    }
+
+    /// test execution
+    Frame * resultFrame = nullptr;
+
+    select(resultFrame, f, "R.a", CompareOperation::Equal, selectValue);
+
+    /// test if result matches expected result
+    CHECK(*resultFrame == *expectedResult);
+
+    /// cleanup
+    DataObjectFactory::destroy(resultFrame, expectedResult, f);
+
+}
+
+TEST_CASE("Morphstore Select: Test the NotEqual (!=) operation", TAG_KERNELS) {
+    /// Data generation
+    auto lhs_col0 = genGivenVals<DenseMatrix<uint64_t>>(10, { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9});
+    auto lhs_col1 = genGivenVals<DenseMatrix<uint64_t>>(10,  { 0, 11, 20, 33, 44, 55, 60, 77, 88, 99});
+    std::vector<Structure *> lhsCols = {lhs_col0, lhs_col1};
+    std::string lhsLabels[] = {"R.idx", "R.a"};
+    auto f = DataObjectFactory::create<Frame>(lhsCols, lhsLabels);
+
+    size_t selectValue = 55;
+
+    Frame * expectedResult;
+    /// create expected result set
+    {
+        std::vector<uint64_t> er_col0_val;
+        std::vector<uint64_t> er_col1_val;
+        for (uint64_t i = 0; i < lhs_col0->getNumRows(); ++ i) {
+            /// condition to check
+            if (lhs_col1->get(i, 0) != selectValue) {
+                er_col0_val.push_back(lhs_col0->get(i, 0));
+                er_col1_val.push_back(lhs_col1->get(i, 0));
+            }
+        }
+        uint64_t size = er_col0_val.size();
+        auto er_col0 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col0_val);
+        auto er_col1 = genGivenVals<DenseMatrix<uint64_t>>(size, er_col1_val);
+        /// create result data
+        expectedResult = DataObjectFactory::create<Frame>(
+                std::vector<Structure*>{er_col0,er_col1},
+                lhsLabels);
+        /// cleanup
+        DataObjectFactory::destroy(er_col0, er_col1);
+        DataObjectFactory::destroy(lhs_col0, lhs_col1);
+    }
+
+    /// test execution
+    Frame * resultFrame = nullptr;
+
+    select(resultFrame, f, "R.a", CompareOperation::NotEqual, selectValue);
+
+    /// test if result matches expected result
+    CHECK(*resultFrame == *expectedResult);
+
+    /// cleanup
+    DataObjectFactory::destroy(resultFrame, expectedResult, f);
+
 }
