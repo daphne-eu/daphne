abb1501212be39802b13c55c1cc7be5420567027
AhmedEleliemy <eleliemy.ahmedhamdy@gmail.com>
2022-10-01 09:00:36 +0200

    Put back ProtoDataConverter to its orignial, fixing the linking issue is done by adding a rule for compuling daphne executable

--- src/runtime/distributed/proto/ProtoDataConverter.cpp
+++ src/runtime/distributed/proto/ProtoDataConverter.cpp
@@ -47,8 +47,7 @@ void ProtoDataConverter<DenseMatrix<VT>>::convertToProto(const DenseMatrix<VT> *
 {
     convertToProto(mat, matProto, 0, mat->getNumRows(), 0, mat->getNumCols());
 }
-
-/*template<typename VT>
+template<typename VT>
 void ProtoDataConverter<DenseMatrix<VT>>::convertFromProto(const distributed::Matrix &matProto,
                                           DenseMatrix<VT> *mat,
                                           size_t rowBegin,
@@ -71,7 +70,6 @@ void ProtoDataConverter<DenseMatrix<VT>>::convertFromProto(const distributed::Ma
 {
     convertFromProto(matProto, mat, 0, mat->getNumRows(), 0, mat->getNumCols());
 }
-*/
 template<>
 google::protobuf::RepeatedField<int64_t> *ProtoDataConverter<DenseMatrix<int64_t>>::getMutableCells(distributed::Matrix *matProto)
 {
