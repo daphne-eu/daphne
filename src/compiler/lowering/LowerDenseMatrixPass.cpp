2017d976d7d9c764d2ceef2c19cdf7e09f6963fd
philipportner <philipp.ortner@student.tugraz.at>
2023-03-08 10:08:13 +0100

    EwBinaryModOp codegen

--- src/compiler/lowering/LowerDenseMatrixPass.cpp
+++ src/compiler/lowering/LowerDenseMatrixPass.cpp
@@ -35,6 +35,7 @@
 #include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
 #include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/Transforms/FuncConversions.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
@@ -58,9 +59,11 @@ constexpr int COL = 1;
 
 void affineFillMemRef(double value, ConversionPatternRewriter &rewriter,
                       mlir::Location loc, ArrayRef<int64_t> shape,
-                      mlir::MLIRContext *ctx, mlir::Value memRef, mlir::Type elemType) {
+                      mlir::MLIRContext *ctx, mlir::Value memRef,
+                      mlir::Type elemType) {
     Value fillValue = rewriter.create<mlir::arith::ConstantOp>(
-        loc, elemType, rewriter.getFloatAttr(elemType, value));
+        loc, rewriter.getF64Type(), rewriter.getF64FloatAttr(value));
+
     SmallVector<Value, 4> loopIvs;
 
     auto outerLoop = rewriter.create<AffineForOp>(loc, 0, shape[ROW], 1);
@@ -225,6 +228,101 @@ class MatMulOpLowering : public OpConversionPattern<daphne::MatMulOp> {
     }
 };
 
+class EwModOpLowering : public OpConversionPattern<daphne::EwModOp> {
+   public:
+    using OpConversionPattern::OpConversionPattern;
+
+    LogicalResult matchAndRewrite(
+        daphne::EwModOp op, OpAdaptor adaptor,
+        ConversionPatternRewriter &rewriter) const override {
+        auto loc = op->getLoc();
+        mlir::daphne::MatrixType lhsTensor =
+            adaptor.getLhs().getType().dyn_cast<mlir::daphne::MatrixType>();
+
+        auto lhsRows = lhsTensor.getNumRows();
+        auto lhsCols = lhsTensor.getNumCols();
+
+        auto tensorType = lhsTensor.getElementType();
+        auto lhsMemRefType =
+            mlir::MemRefType::get({lhsRows, lhsCols}, tensorType);
+
+        mlir::MemRefType outputMemRefType =
+            mlir::MemRefType::get({lhsCols, lhsRows}, tensorType);
+
+        // daphne::Matrix -> memref
+        mlir::Value lhs = rewriter.create<mlir::daphne::GetMemRefDenseMatrix>(
+            op->getLoc(), lhsMemRefType, adaptor.getLhs());
+        mlir::Value rhs = adaptor.getRhs();
+
+        Value cst_one = rewriter.create<mlir::arith::ConstantOp>(
+                loc, rewriter.getF64Type(), rewriter.getF64FloatAttr(1));
+
+        mlir::Value rhsValue =
+            rewriter.create<arith::SubFOp>(loc, rhs, cst_one);
+        mlir::Value rhsV = rewriter.create<mlir::arith::FPToSIOp>(loc, rewriter.getI64Type(), rhsValue);
+
+        // Alloc output memref
+        mlir::Value outputMemRef =
+            rewriter.create<memref::AllocOp>(loc, outputMemRefType);
+
+        // Fill the output MemRef
+        affineFillMemRef(0.0, rewriter, loc, outputMemRefType.getShape(),
+                         op->getContext(), outputMemRef, tensorType);
+
+        // rewriter.create<linalg::ElemwiseBinaryOp>(loc, ValueRange{lhs, rhs}, ValueRange{outputMemRef}, linalg::BinaryFnAttr::get);
+        SmallVector<Value, 4> loopIvs;
+
+        auto outerLoop = rewriter.create<AffineForOp>(loc, 0, lhsRows, 1);
+        for (Operation &nested : *outerLoop.getBody()) {
+            rewriter.eraseOp(&nested);
+        }
+        loopIvs.push_back(outerLoop.getInductionVar());
+
+        // outer loop body
+        rewriter.setInsertionPointToStart(outerLoop.getBody());
+        auto innerLoop = rewriter.create<AffineForOp>(loc, 0, lhsCols, 1);
+        for (Operation &nested : *innerLoop.getBody()) {
+            rewriter.eraseOp(&nested);
+        }
+        loopIvs.push_back(innerLoop.getInductionVar());
+        rewriter.create<AffineYieldOp>(loc);
+        rewriter.setInsertionPointToStart(innerLoop.getBody());
+        mlir::Value lhsValue = rewriter.create<AffineLoadOp>(loc, lhs, loopIvs);
+        // mlir::Value lhsV = rewriter.create<mlir::arith::FPToSIOp>(loc, rewriter.getI64Type(), lhsValue);
+
+
+        mlir::Value modResult =
+            rewriter.create<arith::AndIOp>(loc, lhsValue, rhsV);
+        mlir::Value modResultCast = rewriter.create<arith::SIToFPOp>(loc, rewriter.getF64Type(), modResult);
+
+        rewriter.create<AffineStoreOp>(loc, modResultCast, outputMemRef, loopIvs);
+
+        rewriter.create<AffineYieldOp>(loc);
+        rewriter.setInsertionPointAfter(outerLoop);
+
+        auto extractStridedMetadataOp =
+            rewriter.create<memref::ExtractStridedMetadataOp>(loc,
+                                                              outputMemRef);
+        // aligned ptr (memref.data)
+        mlir::Value alignedPtr =
+            rewriter.create<memref::ExtractAlignedPointerAsIndexOp>(
+                loc, outputMemRef);
+        // offset
+        mlir::Value offset = extractStridedMetadataOp.getOffset();
+        // strides
+        mlir::ResultRange strides = extractStridedMetadataOp.getStrides();
+        // sizes
+        mlir::ResultRange sizes = extractStridedMetadataOp.getSizes();
+
+        mlir::Value DM =
+            rewriter.create<mlir::daphne::GetDenseMatrixFromMemRef>(
+                loc, op.getType(), alignedPtr, offset, sizes[0], sizes[1],
+                strides[0], strides[1]);
+        rewriter.replaceOp(op, DM);
+        return success();
+    }
+};
+
 class SumAllOpLowering : public OpConversionPattern<daphne::AllAggSumOp> {
    public:
     using OpConversionPattern::OpConversionPattern;
@@ -333,17 +431,28 @@ void LowerDenseMatrixPass::runOnOperation() {
     target.addLegalOp<mlir::daphne::PrintMemRef>();
     target.addIllegalOp<mlir::daphne::AllAggSumOp>();
     target.addIllegalOp<mlir::daphne::MatMulOp>();
+    typeConverter.addConversion(convertInteger);
+    typeConverter.addConversion(convertFloat);
+    typeConverter.addConversion([](Type type) { return type; });
+    typeConverter.addArgumentMaterialization(materializeCastFromIllegal);
+    typeConverter.addSourceMaterialization(materializeCastToIllegal);
+    typeConverter.addTargetMaterialization(materializeCastFromIllegal);
 
     // typeConverter.addConversion([&](daphne::MatrixType t) {
     //     return mlir::MemRefType::get({t.getNumRows(), t.getNumCols()},
     //                                  t.getElementType());
     // });
 
-    patterns.insert<MatMulOpLowering, SumAllOpLowering>(&getContext());
+    patterns.insert<MatMulOpLowering, SumAllOpLowering, EwModOpLowering>(
+        &getContext());
     auto module = getOperation();
     if (failed(applyPartialConversion(module, target, std::move(patterns)))) {
         signalPassFailure();
     }
+}
+
+std::unique_ptr<mlir::Pass> mlir::daphne::createLowerDenseMatrixPass() {
+    return std::make_unique<LowerDenseMatrixPass>();
 }
 
 class MemRefCallingConvention : public OpConversionPattern<daphne::MatMulOp> {
@@ -387,9 +496,9 @@ class MemRefCallingConvention : public OpConversionPattern<daphne::MatMulOp> {
         affineFillMemRef(0.0, rewriter, loc, outputMemRefType.getShape(),
                          op->getContext(), outputMemRef, tensorType);
 
-        rewriter
-            .create<linalg::MatmulOp>(loc, //TypeRange{outputMemRefType},
-                                      ValueRange{lhs, rhs}, ValueRange{outputMemRef});
+        rewriter.create<linalg::MatmulOp>(loc,  // TypeRange{outputMemRefType},
+                                          ValueRange{lhs, rhs},
+                                          ValueRange{outputMemRef});
         auto extractStridedMetadataOp =
             rewriter.create<memref::ExtractStridedMetadataOp>(loc,
                                                               outputMemRef);
@@ -448,10 +557,6 @@ void MemRefCallingConventionPass::runOnOperation() {
     if (failed(applyPartialConversion(module, target, std::move(patterns)))) {
         signalPassFailure();
     }
-}
-
-std::unique_ptr<mlir::Pass> mlir::daphne::createLowerDenseMatrixPass() {
-    return std::make_unique<LowerDenseMatrixPass>();
 }
 
 std::unique_ptr<mlir::Pass> mlir::daphne::createMemRefTestPass() {
