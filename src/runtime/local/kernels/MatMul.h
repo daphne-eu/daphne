7a480636f97dfb8bbdccbfdf8e32ae7b8ffc1d42
philipportner <philipp.ortner@student.tugraz.at>
2023-03-14 14:32:38 +0100

    handwritten mm to mimic mlir codegen mm

--- src/runtime/local/kernels/MatMul.h
+++ src/runtime/local/kernels/MatMul.h
@@ -86,7 +86,7 @@ struct MatMul<DenseMatrix<double>, DenseMatrix<double>, DenseMatrix<double>> {
         assert((nc1 == static_cast<int>(transb ? rhs->getNumCols() : rhs->getNumRows())) && "#cols of lhs and #rows of rhs must be the same");
         
         if(res == nullptr)
-            res = DataObjectFactory::create<DenseMatrix<double>>(nr1, nc2, false);
+            res = DataObjectFactory::create<DenseMatrix<double>>(nr1, nc2, true);
 
         if(nr1 == 1 && nc2 == 1) // Vector-Vector
             res->set(0, 0, cblas_ddot(nc1, lhs->getValues(), 1, rhs->getValues(),
@@ -97,8 +97,22 @@ struct MatMul<DenseMatrix<double>, DenseMatrix<double>, DenseMatrix<double>> {
                 static_cast<int>(rhs->getRowSkip()), 0, res->getValues(),
                 static_cast<int>(res->getRowSkip()));
         else                     // Matrix-Matrix
-            cblas_dgemm(CblasRowMajor, transa ? CblasTrans : CblasNoTrans, transb ? CblasTrans : CblasNoTrans, nr1, nc2, nc1,
-                1, lhs->getValues(), static_cast<int>(lhs->getRowSkip()), rhs->getValues(),
-                static_cast<int>(rhs->getRowSkip()), 0, res->getValues(), static_cast<int>(res->getRowSkip()));
+        {
+            int N = nr1;
+            double *A = (double*)lhs->getValues();
+            double *B = (double*)rhs->getValues();
+            double *C = (double*)res->getValues();
+            for (int row = 0; row < N; ++row) {
+                for (int inner = 0; inner < N; ++inner) {
+                    for (int col = 0; col < N; ++col) {
+                        C[row * N + col] += A[row * N + inner] * B[inner * N + col];
+                    }
+                }
+            }
+            // cblas_dgemm(CblasRowMajor, transa ? CblasTrans : CblasNoTrans, transb ? CblasTrans : CblasNoTrans, nr1, nc2, nc1,
+            //     1, lhs->getValues(), static_cast<int>(lhs->getRowSkip()), rhs->getValues(),
+            //     static_cast<int>(rhs->getRowSkip()), 0, res->getValues(), static_cast<int>(res->getRowSkip()));
+
+        }
     }
 };
