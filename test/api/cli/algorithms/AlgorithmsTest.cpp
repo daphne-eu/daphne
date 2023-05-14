54025705347d0f6457152cd75d57b6a3852770f4
Patrick Damme <patrick.damme@tu-berlin.de>
2023-05-14 21:35:12 +0200

    New DaphneDSL scripts for linear regression model training.
    - Added scripts for linear regression model training (LM) using the direct solve (DS) and conjugate gradient (CG) methods.
      - We already had a simplified script for lmDS in the test cases, which is retained.
      - We already had a first attempt at the translation of Apache SystemDS's lmDS script, which is replaced.
      - Newly translated Apache SystemDS's lmDS and lmCG from scratch (meanwhile, many issues hindering that have been resolved).
      - Separation of DaphneDSL scripts containing just the lmDS and lmCG functions (to be imported into other DaphneDSL scripts) and scripts calling those functions on input files.
    - Some little changes to the DaphneDSL parser and kernel instantiations, which were necessary to make it work.
    - Script-level test cases checking the output of lmDS and lmCG for all three intercept options as well as for non-vectorized and vectorized processing.
    - Updated the readme on the available algorithms.

--- test/api/cli/algorithms/AlgorithmsTest.cpp
+++ test/api/cli/algorithms/AlgorithmsTest.cpp
@@ -57,3 +57,32 @@ TEST_CASE("lm", TAG_ALGORITHMS) {
             "--args", "r=100,c=20"
     );
 }
+
+/**
+ * @brief Checks if the "direct solve" and the "conjugate gradient" methods for
+ * linear regression model training (lmDS and lmCG) yield (approximately) the
+ * same results (i) as each other, and (ii) for non-vectorized and vectorized
+ * processing.
+ * 
+ * This is tested for all three options for the intercept, but the results
+ * obtained with different intercept values are not compared to each other.
+ */
+TEST_CASE("lmDS-lmCG", TAG_ALGORITHMS) {
+    for(int icpt : {0, 1, 2}) {
+        DYNAMIC_SECTION("icpt=" << icpt) {
+            // Non-vectorized (no --vec).
+            checkDaphneStatusCode(
+                    StatusCode::SUCCESS, dirPath + "lmDS-lmCG.daphne",
+                    "--args", ("r=100,c=20,icpt=" + std::to_string(icpt) + ",write=true,suffix=\"__novec\"").c_str()
+            );
+            // Vectorized (--vec).
+            checkDaphneStatusCode(
+                    StatusCode::SUCCESS, dirPath + "lmDS-lmCG.daphne",
+                    "--args", ("r=100,c=20,icpt=" + std::to_string(icpt) + ",write=true,suffix=\"__vec\"").c_str(),
+                    "--vec"
+            );
+            // Compare DS to CG and non-vectorized to vectorized.
+            compareDaphneToStr("1\n1\n1\n", dirPath + "lmDS-lmCG_check.daphne");
+        }
+    }
+}
