2adc4956132ddb5596949cb26caced90f937bff4
Eric Mier <eric.mier@tu-dresden.de>
2023-05-30 19:13:15 +0200

    build: Removed unintentional fi

--- build.sh
+++ build.sh
@@ -930,18 +930,19 @@ if [ $WITH_DEPS -gt 0 ]; then
     # the LLVM commit hash we built into a file, and only rebuild MLIR/LLVM if this
     # file does not exist (first build of the prototype) or does not contain the
     # expected hash (upgrade of the LLVM sub-module).
-#------------------------------------------------------------------------------
-# TSL (Template SIMD Library)
-#------------------------------------------------------------------------------
-if ! is_dependency_installed "TSL"; then
-    daphne_msg "Install TSL."
-    tsl_generator="${sourcePrefix}/TSLGenerator/main.py"
-    tsl_output="${installPrefix}/include/TSL"
-    python3 ${tsl_generator} --no-workaround-warnings -o ${tsl_output}
-    dependency_install_success "TSL"
-else
-    daphne_msg "No need to generate TSL again."
-fi
+    #------------------------------------------------------------------------------
+    # TSL (Template SIMD Library)
+    #------------------------------------------------------------------------------
+    if ! is_dependency_installed "TSL"; then
+        daphne_msg "Install TSL."
+        tsl_generator="${sourcePrefix}/TSLGenerator"
+        tsl_output="${installPrefix}/include/TSL"
+        # python3 ${tsl_generator} --no-workaround-warnings -o ${tsl_output}
+        cmake ${tsl_generator} -B ${tsl_generator}/build -D GENERATOR_OUTPUT_PATH=${tsl_output}
+        dependency_install_success "TSL"
+    else
+        daphne_msg "No need to generate TSL again."
+    fi
 
 
 
