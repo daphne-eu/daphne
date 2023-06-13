4359f98be84f0f25bb22818bf8376c561747a153
Patrick Damme <patrick.damme@tu-berlin.de>
2023-06-13 20:14:06 +0200

    [MINOR] Fixed indentation in ReadDaphne.h and WriteDaphne.h.
    - Tab to 4*space.

--- src/runtime/local/io/ReadDaphne.h
+++ src/runtime/local/io/ReadDaphne.h
@@ -43,9 +43,8 @@
 // ****************************************************************************
 
 template <class DTRes>
-struct ReadDaphne
-{
-	static void apply(DTRes *&res, const char *filename) = delete;
+struct ReadDaphne {
+    static void apply(DTRes *&res, const char *filename) = delete;
 };
 
 // ****************************************************************************
@@ -53,9 +52,8 @@ struct ReadDaphne
 // ****************************************************************************
 
 template <class DTRes>
-void readDaphne(DTRes *&res, const char *filename)
-{
-	ReadDaphne<DTRes>::apply(res, filename);
+void readDaphne(DTRes *&res, const char *filename) {
+    ReadDaphne<DTRes>::apply(res, filename);
 }
 
 // ****************************************************************************
@@ -63,146 +61,139 @@ void readDaphne(DTRes *&res, const char *filename)
 // ****************************************************************************
 
 template <typename VT>
-struct ReadDaphne<DenseMatrix<VT>>
-{
-	static void apply(DenseMatrix<VT> *&res, const char *filename)
-	{
-		std::ifstream f;
-		f.open(filename, std::ios::in | std::ios::binary);
-		// TODO: check f.good()
+struct ReadDaphne<DenseMatrix<VT>> {
+    static void apply(DenseMatrix<VT> *&res, const char *filename) {
+        std::ifstream f;
+        f.open(filename, std::ios::in | std::ios::binary);
+        // TODO: check f.good()
 
-		auto deser = DaphneDeserializerChunks<DenseMatrix<VT>>(&res, DaphneSerializer<DenseMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE);
-		for (auto it = deser.begin(); it != deser.end(); ++it) {
-			it->first = DaphneSerializer<DenseMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE;
-			f.read(it->second.data(), it->first);
-			// in case we read less than that
-			it->first = f.gcount();
-		}
+        auto deser = DaphneDeserializerChunks<DenseMatrix<VT>>(&res, DaphneSerializer<DenseMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE);
+        for (auto it = deser.begin(); it != deser.end(); ++it) {
+            it->first = DaphneSerializer<DenseMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE;
+            f.read(it->second.data(), it->first);
+            // in case we read less than that
+            it->first = f.gcount();
+        }
 
-		f.close();
-		return;
-	}
+        f.close();
+        return;
+    }
 };
 
 template <typename VT>
-struct ReadDaphne<CSRMatrix<VT>>
-{
-	static void apply(CSRMatrix<VT> *&res, const char *filename)
-	{
-		std::ifstream f;
-		f.open(filename, std::ios::in | std::ios::binary);
-		// TODO: check f.good()
+struct ReadDaphne<CSRMatrix<VT>> {
+    static void apply(CSRMatrix<VT> *&res, const char *filename) {
+        std::ifstream f;
+        f.open(filename, std::ios::in | std::ios::binary);
+        // TODO: check f.good()
 
-		auto deser = DaphneDeserializerChunks<CSRMatrix<VT>>(&res, DaphneSerializer<CSRMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE);
-		for (auto it = deser.begin(); it != deser.end(); ++it) {
-			it->first = DaphneSerializer<CSRMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE;
-			f.read(it->second.data(), it->first);
-			// in case we read less than that
-			it->first = f.gcount();
-		}
+        auto deser = DaphneDeserializerChunks<CSRMatrix<VT>>(&res, DaphneSerializer<CSRMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE);
+        for (auto it = deser.begin(); it != deser.end(); ++it) {
+            it->first = DaphneSerializer<CSRMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE;
+            f.read(it->second.data(), it->first);
+            // in case we read less than that
+            it->first = f.gcount();
+        }
 
-		f.close();
-		return;
-	}
+        f.close();
+        return;
+    }
 };
 
 template <>
-struct ReadDaphne<Frame>
-{
-	static void apply(Frame *&res, const char *filename)
-	{
-		std::ifstream f;
-		f.open(filename, std::ios::in | std::ios::binary);
-		// TODO: check f.good()
+struct ReadDaphne<Frame> {
+    static void apply(Frame *&res, const char *filename) {
+        std::ifstream f;
+        f.open(filename, std::ios::in | std::ios::binary);
+        // TODO: check f.good()
 
-		// read commong part of the header
-		DF_header h;
-		f.read((char *)&h, sizeof(h));
+        // read commong part of the header
+        DF_header h;
+        f.read((char *)&h, sizeof(h));
 
-		if (h.dt == (uint8_t)DF_data_t::Frame_t) {
-			// read rest of the header
-			ValueTypeCode *schema = new ValueTypeCode[h.nbcols];
-			for (uint64_t c = 0; c < h.nbcols; c++) {
-				f.read((char *)&(schema[c]), sizeof(ValueTypeCode));
-			}
+        if (h.dt == (uint8_t)DF_data_t::Frame_t) {
+            // read rest of the header
+            ValueTypeCode *schema = new ValueTypeCode[h.nbcols];
+            for (uint64_t c = 0; c < h.nbcols; c++) {
+                f.read((char *)&(schema[c]), sizeof(ValueTypeCode));
+            }
 
-			std::string *labels = new std::string[h.nbcols];
-			for (uint64_t c = 0; c < h.nbcols; c++) {
-				uint16_t len;
-				f.read((char *)&len, sizeof(len));
-				f.read((char *)&(labels[c]), len);
-			}
+            std::string *labels = new std::string[h.nbcols];
+            for (uint64_t c = 0; c < h.nbcols; c++) {
+                uint16_t len;
+                f.read((char *)&len, sizeof(len));
+                f.read((char *)&(labels[c]), len);
+            }
 
-			DF_body b;
-			f.read((char *)&b, sizeof(b));
-			// b is ignored for now - assumed to be 0,0
-			// TODO: consider multiple blocks
-			// Assuming a dense block representation
-			// TODO: Consider alternative representations for frames
+            DF_body b;
+            f.read((char *)&b, sizeof(b));
+            // b is ignored for now - assumed to be 0,0
+            // TODO: consider multiple blocks
+            // Assuming a dense block representation
+            // TODO: Consider alternative representations for frames
 
-			if (res == nullptr) {
-				res = DataObjectFactory::create<Frame>(h.nbrows, h.nbcols, schema, nullptr, false);
-			}
+            if (res == nullptr) {
+                res = DataObjectFactory::create<Frame>(h.nbrows, h.nbcols, schema, nullptr, false);
+            }
 
-			uint8_t **rawCols = new uint8_t *[h.nbcols];
-			for (size_t i = 0; i < h.nbcols; i++) {
-				rawCols[i] = reinterpret_cast<uint8_t *>(res->getColumnRaw(i));
-			}
+            uint8_t **rawCols = new uint8_t *[h.nbcols];
+            for (size_t i = 0; i < h.nbcols; i++) {
+                rawCols[i] = reinterpret_cast<uint8_t *>(res->getColumnRaw(i));
+            }
 
-			for (size_t r = 0; r < h.nbrows; r++) {
-				for (size_t c = 0; c < h.nbcols; c++) {
-					switch (schema[c]) 
-					{
-					case ValueTypeCode::SI8:
-						int8_t val_si8;
-						f.read((char *)&val_si8, sizeof(val_si8));
-						reinterpret_cast<int8_t *>(rawCols[c])[r] = val_si8;
-						break;
-					case ValueTypeCode::SI32:
-						int32_t val_si32;
-						f.read((char *)&val_si32, sizeof(val_si32));
-						reinterpret_cast<int32_t *>(rawCols[c])[r] = val_si32;
-						break;
-					case ValueTypeCode::SI64:
-						int64_t val_si64;
-						f.read((char *)&val_si64, sizeof(val_si64));
-						reinterpret_cast<int64_t *>(rawCols[c])[r] = val_si64;
-						break;
-					case ValueTypeCode::UI8:
-						uint8_t val_ui8;
-						f.read((char *)&val_ui8, sizeof(val_ui8));
-						reinterpret_cast<uint8_t *>(rawCols[c])[r] = val_ui8;
-						break;
-					case ValueTypeCode::UI32:
-						uint32_t val_ui32;
-						f.read((char *)&val_ui32, sizeof(val_ui32));
-						reinterpret_cast<uint32_t *>(rawCols[c])[r] = val_ui32;
-						break;
-					case ValueTypeCode::UI64:
-						uint64_t val_ui64;
-						f.read((char *)&val_ui64, sizeof(val_ui64));
-						reinterpret_cast<uint64_t *>(rawCols[c])[r] = val_ui64;
-						break;
-					case ValueTypeCode::F32:
-						float val_f32;
-						f.read((char *)&val_f32, sizeof(val_f32));
-						reinterpret_cast<float *>(rawCols[c])[r] = val_f32;
-						break;
-					case ValueTypeCode::F64:
-						double val_f64;
-						f.read((char *)&val_f64, sizeof(val_f64));
-						reinterpret_cast<double *>(rawCols[c])[r] = val_f64;
-						break;
-					default:
-						throw std::runtime_error("ReadDaphne::apply: unknown value type code");
-					}
-				}
-			}
+            for (size_t r = 0; r < h.nbrows; r++) {
+                for (size_t c = 0; c < h.nbcols; c++) {
+                    switch (schema[c]) {
+                        case ValueTypeCode::SI8:
+                            int8_t val_si8;
+                            f.read((char *)&val_si8, sizeof(val_si8));
+                            reinterpret_cast<int8_t *>(rawCols[c])[r] = val_si8;
+                            break;
+                        case ValueTypeCode::SI32:
+                            int32_t val_si32;
+                            f.read((char *)&val_si32, sizeof(val_si32));
+                            reinterpret_cast<int32_t *>(rawCols[c])[r] = val_si32;
+                            break;
+                        case ValueTypeCode::SI64:
+                            int64_t val_si64;
+                            f.read((char *)&val_si64, sizeof(val_si64));
+                            reinterpret_cast<int64_t *>(rawCols[c])[r] = val_si64;
+                            break;
+                        case ValueTypeCode::UI8:
+                            uint8_t val_ui8;
+                            f.read((char *)&val_ui8, sizeof(val_ui8));
+                            reinterpret_cast<uint8_t *>(rawCols[c])[r] = val_ui8;
+                            break;
+                        case ValueTypeCode::UI32:
+                            uint32_t val_ui32;
+                            f.read((char *)&val_ui32, sizeof(val_ui32));
+                            reinterpret_cast<uint32_t *>(rawCols[c])[r] = val_ui32;
+                            break;
+                        case ValueTypeCode::UI64:
+                            uint64_t val_ui64;
+                            f.read((char *)&val_ui64, sizeof(val_ui64));
+                            reinterpret_cast<uint64_t *>(rawCols[c])[r] = val_ui64;
+                            break;
+                        case ValueTypeCode::F32:
+                            float val_f32;
+                            f.read((char *)&val_f32, sizeof(val_f32));
+                            reinterpret_cast<float *>(rawCols[c])[r] = val_f32;
+                            break;
+                        case ValueTypeCode::F64:
+                            double val_f64;
+                            f.read((char *)&val_f64, sizeof(val_f64));
+                            reinterpret_cast<double *>(rawCols[c])[r] = val_f64;
+                            break;
+                        default:
+                            throw std::runtime_error("ReadDaphne::apply: unknown value type code");
+                    }
+                }
+            }
 
-			delete[] rawCols;
-			delete[] schema;
-		}
-		f.close();
-		return;
-	}
+            delete[] rawCols;
+            delete[] schema;
+        }
+        f.close();
+        return;
+    }
 };
--- src/runtime/local/io/WriteDaphne.h
+++ src/runtime/local/io/WriteDaphne.h
@@ -42,9 +42,8 @@
 // ****************************************************************************
 
 template <class DTArg>
-struct WriteDaphne
-{
-	static void apply(const DTArg *arg, const char *filename) = delete;
+struct WriteDaphne {
+    static void apply(const DTArg *arg, const char *filename) = delete;
 };
 
 // ****************************************************************************
@@ -52,9 +51,8 @@ struct WriteDaphne
 // ****************************************************************************
 
 template <class DTArg>
-void writeDaphne(const DTArg *arg, const char *filename)
-{
-	WriteDaphne<DTArg>::apply(arg, filename);
+void writeDaphne(const DTArg *arg, const char *filename) {
+    WriteDaphne<DTArg>::apply(arg, filename);
 }
 
 // ****************************************************************************
@@ -66,22 +64,20 @@ void writeDaphne(const DTArg *arg, const char *filename)
 // ----------------------------------------------------------------------------
 
 template <typename VT>
-struct WriteDaphne<DenseMatrix<VT>>
-{
-	static void apply(const DenseMatrix<VT> *arg, const char *filename)
-	{
-		std::ofstream f;
-		f.open(filename, std::ios::out | std::ios::binary);
-		// TODO: check f.good()
+struct WriteDaphne<DenseMatrix<VT>> {
+    static void apply(const DenseMatrix<VT> *arg, const char *filename) {
+        std::ofstream f;
+        f.open(filename, std::ios::out | std::ios::binary);
+        // TODO: check f.good()
 
-		auto ser = DaphneSerializerChunks<const DenseMatrix<VT>>(arg, DaphneSerializer<DenseMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE);
-		for (auto it = ser.begin(); it != ser.end(); ++it) {
-			f.write(it->second.data(), it->first);
-		}
+        auto ser = DaphneSerializerChunks<const DenseMatrix<VT>>(arg, DaphneSerializer<DenseMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE);
+        for (auto it = ser.begin(); it != ser.end(); ++it) {
+            f.write(it->second.data(), it->first);
+        }
 
-		f.close();
-		return;
-	}
+        f.close();
+        return;
+    }
 };
 
 // ----------------------------------------------------------------------------
@@ -89,22 +85,20 @@ struct WriteDaphne<DenseMatrix<VT>>
 // ----------------------------------------------------------------------------
 
 template <typename VT>
-struct WriteDaphne<CSRMatrix<VT>>
-{
-	static void apply(const CSRMatrix<VT> *arg, const char *filename)
-	{
-		std::ofstream f;
-		f.open(filename, std::ios::out | std::ios::binary);
-		// TODO: check f.good()
+struct WriteDaphne<CSRMatrix<VT>> {
+    static void apply(const CSRMatrix<VT> *arg, const char *filename) {
+        std::ofstream f;
+        f.open(filename, std::ios::out | std::ios::binary);
+        // TODO: check f.good()
 
-		auto ser = DaphneSerializerChunks<const CSRMatrix<VT>>(arg, DaphneSerializer<CSRMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE);
-		for (auto it = ser.begin(); it != ser.end(); ++it) {
-			f.write(it->second.data(), it->first);
-		}
+        auto ser = DaphneSerializerChunks<const CSRMatrix<VT>>(arg, DaphneSerializer<CSRMatrix<VT>>::DEFAULT_SERIALIZATION_BUFFER_SIZE);
+        for (auto it = ser.begin(); it != ser.end(); ++it) {
+            f.write(it->second.data(), it->first);
+        }
 
-		f.close();
-		return;
-	}
+        f.close();
+        return;
+    }
 };
 
 // ----------------------------------------------------------------------------
@@ -112,84 +106,81 @@ struct WriteDaphne<CSRMatrix<VT>>
 // ----------------------------------------------------------------------------
 
 template <>
-struct WriteDaphne<Frame>
-{
-	static void apply(const Frame *arg, const char *filename)
-	{
+struct WriteDaphne<Frame> {
+    static void apply(const Frame *arg, const char *filename) {
 
-		std::ofstream f;
-		f.open(filename, std::ios::out | std::ios::binary);
-		// TODO: check f.good()
+        std::ofstream f;
+        f.open(filename, std::ios::out | std::ios::binary);
+        // TODO: check f.good()
 
-		// write header
-		DF_header h;
-		h.version = 1;
-		h.dt = (uint8_t)DF_data_t::Frame_t;
-		h.nbrows = (uint64_t)arg->getNumRows();
-		h.nbcols = (uint64_t)arg->getNumCols();
-		f.write((const char *)&h, sizeof(h));
+        // write header
+        DF_header h;
+        h.version = 1;
+        h.dt = (uint8_t)DF_data_t::Frame_t;
+        h.nbrows = (uint64_t)arg->getNumRows();
+        h.nbcols = (uint64_t)arg->getNumCols();
+        f.write((const char *)&h, sizeof(h));
 
-		const ValueTypeCode *schema = arg->getSchema();
-		const std::string *labels = arg->getLabels();
+        const ValueTypeCode *schema = arg->getSchema();
+        const std::string *labels = arg->getLabels();
 
-		for (uint64_t c = 0; c < h.nbcols; c++) {
-			f.write((const char *)&(schema[c]), sizeof(ValueTypeCode));
-		}
+        for (uint64_t c = 0; c < h.nbcols; c++) {
+            f.write((const char *)&(schema[c]), sizeof(ValueTypeCode));
+        }
 
-		for (uint64_t c = 0; c < h.nbcols; c++) {
-			uint16_t len = (labels[c]).length();
-			f.write((const char *)&len, sizeof(len));
-			f.write((const char *)&(labels[c]), len);
-		}
+        for (uint64_t c = 0; c < h.nbcols; c++) {
+            uint16_t len = (labels[c]).length();
+            f.write((const char *)&len, sizeof(len));
+            f.write((const char *)&(labels[c]), len);
+        }
 
-		DF_body b;
-		b.rx = 0;
-		b.cx = 0;
-		f.write((char *)&b, sizeof(b));
-		// TODO: consider multiple blocks
-		//  Assuming a dense block representation
-		//  TODO: Consider alternative representations for frames
+        DF_body b;
+        b.rx = 0;
+        b.cx = 0;
+        f.write((char *)&b, sizeof(b));
+        // TODO: consider multiple blocks
+        //  Assuming a dense block representation
+        //  TODO: Consider alternative representations for frames
 
-		void *vals[h.nbcols];
-		for (size_t c = 0; c < h.nbcols; c++) {
-			vals[c] = const_cast<void *>(arg->getColumnRaw(c));
-		}
+        void *vals[h.nbcols];
+        for (size_t c = 0; c < h.nbcols; c++) {
+            vals[c] = const_cast<void *>(arg->getColumnRaw(c));
+        }
 
-		for (size_t r = 0; r < h.nbrows; r++) {
-			for (size_t c = 0; c < h.nbcols; c++) {
-				switch (schema[c])
-				{
-				case ValueTypeCode::SI8:
-					f.write((char *)&(reinterpret_cast<int8_t *>(vals[c])[r]), sizeof(int8_t));
-					break;
-				case ValueTypeCode::SI32:
-					f.write((char *)&(reinterpret_cast<int32_t *>(vals[c])[r]), sizeof(int32_t));
-					break;
-				case ValueTypeCode::SI64:
-					f.write((char *)&(reinterpret_cast<int64_t *>(vals[c])[r]), sizeof(int64_t));
-					break;
-				case ValueTypeCode::UI8:
-					f.write((char *)&(reinterpret_cast<uint8_t *>(vals[c])[r]), sizeof(uint8_t));
-					break;
-				case ValueTypeCode::UI32:
-					f.write((char *)&(reinterpret_cast<uint32_t *>(vals[c])[r]), sizeof(uint32_t));
-					break;
-				case ValueTypeCode::UI64:
-					f.write((char *)&(reinterpret_cast<uint64_t *>(vals[c])[r]), sizeof(uint64_t));
-					break;
-				case ValueTypeCode::F32:
-					f.write((char *)&(reinterpret_cast<float *>(vals[c])[r]), sizeof(float));
-					break;
-				case ValueTypeCode::F64:
-					f.write((char *)&(reinterpret_cast<double *>(vals[c])[r]), sizeof(double));
-					break;
-				default:
-					throw std::runtime_error("WriteDaphne::apply: unknown value type code");
-				}
-			}
-		}
+        for (size_t r = 0; r < h.nbrows; r++) {
+            for (size_t c = 0; c < h.nbcols; c++) {
+                switch (schema[c]) {
+                    case ValueTypeCode::SI8:
+                        f.write((char *)&(reinterpret_cast<int8_t *>(vals[c])[r]), sizeof(int8_t));
+                        break;
+                    case ValueTypeCode::SI32:
+                        f.write((char *)&(reinterpret_cast<int32_t *>(vals[c])[r]), sizeof(int32_t));
+                        break;
+                    case ValueTypeCode::SI64:
+                        f.write((char *)&(reinterpret_cast<int64_t *>(vals[c])[r]), sizeof(int64_t));
+                        break;
+                    case ValueTypeCode::UI8:
+                        f.write((char *)&(reinterpret_cast<uint8_t *>(vals[c])[r]), sizeof(uint8_t));
+                        break;
+                    case ValueTypeCode::UI32:
+                        f.write((char *)&(reinterpret_cast<uint32_t *>(vals[c])[r]), sizeof(uint32_t));
+                        break;
+                    case ValueTypeCode::UI64:
+                        f.write((char *)&(reinterpret_cast<uint64_t *>(vals[c])[r]), sizeof(uint64_t));
+                        break;
+                    case ValueTypeCode::F32:
+                        f.write((char *)&(reinterpret_cast<float *>(vals[c])[r]), sizeof(float));
+                        break;
+                    case ValueTypeCode::F64:
+                        f.write((char *)&(reinterpret_cast<double *>(vals[c])[r]), sizeof(double));
+                        break;
+                    default:
+                        throw std::runtime_error("WriteDaphne::apply: unknown value type code");
+                }
+            }
+        }
 
-		f.close();
-		return;
-	}
+        f.close();
+        return;
+    }
 };
