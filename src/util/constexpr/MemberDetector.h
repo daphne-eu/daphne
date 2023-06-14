4e6846478c3df311b0e50eba2721ce12242d45f0
Eric Mier <eric.mier@tu-dresden.de>
2023-06-14 14:39:55 +0200

    Small member detector

--- src/util/constexpr/MemberDetector.h
+++ src/util/constexpr/MemberDetector.h
@@ -59,6 +59,26 @@ namespace daphne {
         inline constexpr
         bool has_non_static_method_apply_v = has_member_method_apply_v<T> && !has_static_method_apply_v<T>;
 
+
+        namespace detail {
+            template <class Default, class AlwaysVoid, template<class...> class Op, class... Args>
+            struct detector {
+                using value_t = std::false_type;
+                using type = Default;
+            };
+            
+            template <class Default, template<class...> class Op, class... Args>
+            struct detector<Default, std::void_t<Op<Args...>>, Op, Args...> {
+                using value_t = std::true_type;
+                using type = Op<Args...>;
+            };
+            
+            struct nonesuch{};
+
+            template <template<class...> class Op, class... Args>
+            using is_detected = typename detail::detector<nonesuch, void, Op, Args...>::value_t;
+        } // namespace detail
+
     }
 }
 
