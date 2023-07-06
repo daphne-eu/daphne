a9d894986b0250073710f695f78e4cb666f0601e
Mark Dokter <mark@dokter.cc>
2023-07-06 08:50:50 +0200

    [MINOR] More 0.2-rc2 improvements
    * DaphneLIB goes to src/api/python in release artifact
    * Left a note in dnn-conv2d.daph example script that DNN ops only run on CUDA

--- pack.sh
+++ pack.sh
@@ -138,8 +138,8 @@ if [[ $? == 0 ]];then
   mkdir -p "$PACK_ROOT/bin"
   # shellcheck disable=SC2154
   cp -a "$projectRoot"/{containers,deploy,doc,lib,scripts} "$PACK_ROOT"
-  mkdir -p "$PACK_ROOT"/{api,bin}
-  cp -a "$projectRoot"/src/api/python "$PACK_ROOT"/api/
+  mkdir -p "$PACK_ROOT"/{bin,src/api}
+  cp -a "$projectRoot"/src/api/python "$PACK_ROOT"/src/api/
   cp -a "$projectRoot"/bin/{daphne,DistributedWorker} "$PACK_ROOT"/bin/
   cp -a "$projectRoot"/run-*.sh "$PACK_ROOT"/
   # this assumes that the pack script is run from an environment that has third party deps in /usr/local
