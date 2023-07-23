80b814236cf5210685a2c14b61d542a7b94929ec
philipportner <philipp.ortner@student.tugraz.at>
2023-07-23 20:13:29 +0200

    op fusion bm

--- benchmark/sumall_typecast/run_benchmarks.py
+++ benchmark/sumall_typecast/run_benchmarks.py
@@ -17,19 +17,19 @@ precompiled_tests = [
     "f32_100mb.daphne",
     "f32_1gb.daphne",
     "f32_5gb.daphne",
-    #  "f32_10gb.daphne" runs out of memory!
+    "f32_10gb.daphne" # runs out of memory!
 ]
 
 codegen_tests = [
-    #  "f32_10x10_cg.daphne",
-    #  "f32_1kb_cg.daphne",
-    #  "f32_10kb_cg.daphne",
-    #  "f32_100kb_cg.daphne",
-    #  "f32_1mb_cg.daphne",
-    #  "f32_10mb_cg.daphne",
-    #  "f32_100mb_cg.daphne",
-    #  "f32_1gb_cg.daphne",
-    #  "f32_5gb_cg.daphne",
+    "f32_10x10_cg.daphne",
+    "f32_1kb_cg.daphne",
+    "f32_10kb_cg.daphne",
+    "f32_100kb_cg.daphne",
+    "f32_1mb_cg.daphne",
+    "f32_10mb_cg.daphne",
+    "f32_100mb_cg.daphne",
+    "f32_1gb_cg.daphne",
+    "f32_5gb_cg.daphne",
     "f32_10gb_cg.daphne" # works, does not run out of memory!
 ]
 
@@ -45,9 +45,10 @@ def run_benchmark(file: str):
     for i in range(ROUNDS):
         # precompiled
         popen = subprocess.Popen(
-            ("../../bin/daphne " + file).split(), stdout=subprocess.PIPE
+            ("daphne " + file).split(), stdout=subprocess.PIPE
         )
         popen.wait()
+        result = str(popen.stdout.readline())
         output = str(popen.stdout.read())
         time_spent_precompiled += float(re.findall(r"\d+", output)[0]) / 1e6
 
@@ -63,9 +64,10 @@ def run_codegen(file: str):
     for i in range(ROUNDS):
         # codegen
         popen = subprocess.Popen(
-            ("../../bin/daphne --codegen " + file).split(), stdout=subprocess.PIPE
+            ("daphne --codegen " + file).split(), stdout=subprocess.PIPE
         )
         popen.wait()
+        result = str(popen.stdout.readline())
         output = str(popen.stdout.read())
         time_spent_codegen += float(re.findall(r"\d+", output)[0]) / 1e6
 
@@ -77,29 +79,29 @@ def run_codegen(file: str):
 
 if __name__ == "__main__":
     print(f"Running tests.")
-    #  for test in precompiled_tests:
-    #      run_benchmark(test)
+    for test in precompiled_tests:
+        run_benchmark(test)
     for test in codegen_tests:
         run_codegen(test)
     print(f"Finished tests.\n")
 
-    #  df = pd.DataFrame(
-    #      {
-    #          "test_name": precompiled_tests,
-    #          "precompiled": precompiled_results,
-    #          "codegen": codegen_results,
-    #      },
-    #      columns=["test_name", "precompiled", "codegen"],
-    #  )
-    #  df.to_csv("results.csv", index=False)
+    df = pd.DataFrame(
+        {
+            "test_name": precompiled_tests,
+            "precompiled": precompiled_results,
+            "codegen": codegen_results,
+        },
+        columns=["test_name", "precompiled", "codegen"],
+    )
+    df.to_csv("results.csv", index=False)
 
     # since we can't run codegen and codegen_optimized with the same binary I
     # stitch the results together after a seperate run for codegen_optimized
-    df = pd.DataFrame(
-        {
-            'codegen_optimized': codegen_results
-        }, columns=['codegen_optimized']
-    )
-    df.to_csv('codegen.csv', index=False)
+    #  df = pd.DataFrame(
+    #      {
+    #          'codegen_optimized': codegen_results
+    #      }, columns=['codegen_optimized']
+    #  )
+    #  df.to_csv('codegen.csv', index=False)
 
     print(df)
