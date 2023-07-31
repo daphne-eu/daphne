14b23c0287f419c454b7ed1958f374ac9e0458a6
Niklas <ventker.niklas@gmail.com>
2023-07-31 20:02:59 +0200

    Script update
    Ansatz für eine bessere Auswertung

--- scripts/examples/daphnelib/data-exchange-pandas-performancetest.py
+++ scripts/examples/daphnelib/data-exchange-pandas-performancetest.py
@@ -15,9 +15,17 @@
 from api.python.context.daphne_context import DaphneContext
 import pandas as pd
 import numpy as np
-import string
+import csv
+import time
 
 dc = DaphneContext()
+
+#Create Lists for the PerfoamnceResults Dataframe 
+type = []
+dataframe_size = []
+transfer_daphne_time = []
+rbind_time = []
+cartesian_time = []
 
 # Creating a list of sizes for the objects
 sizes = [1, 10, 100, 1000, 10000, 100000, 1000000]
@@ -29,17 +37,33 @@ dataframes = [pd.DataFrame(np.random.randn(size, 15)) for size in sizes]
 # Looping through the dataframes and testing the from_pandas and compute operation
 print("\n\n###\n### Dataframes Experiments:\n###\n")
 for df in dataframes:
+    type.append("Dataframe")
+
     print("Dataframe Size:",df.size)
+    dataframe_size.append(df.size)
+
     # Transfer data to DaphneLib
+    start_time = time.time()
     F = dc.from_pandas(df, verbose=True)
+    end_time = time.time()
+    transfer_daphne_time.append(end_time-start_time)
+
     # Appending and cartesian calculation
+    r_start_time = time.time()
     F = F.rbind(F)
     F.compute(verbose=True)
+    r_end_time = time.time()
+    rbind_time.append(r_end_time-r_start_time)
+    
+    c_start_time = time.time()
     F = F.cartesian(F)
     F.compute(verbose=True)
+    c_end_time = time.time()
+    rbind_time.append(c_end_time-c_start_time)
+    print(rbind_time)
+
     print(F.compute())
 print("\n###End of Dataframe Experiments.\n")
-
 
 # SERIES
 # Creating a list of series with different sizes
@@ -49,7 +73,7 @@ print("\n\n###\n### Series Experiments:\n###\n")
 for s in series:
     print("Series Size:",s.size)
     # Transfer data to DaphneLib
-    F = dc.from_pandas(s)
+    F = dc.from_pandas(s, verbose=True)
     # Appending and cartesian calculation
     F = F.rbind(F)
     F.compute()
@@ -57,7 +81,6 @@ for s in series:
     F.compute()
     print(F.compute())
 print("\n###End of Series Experiments.\n")
-
 
 # SPARSE DATAFRAME
 # Creating a list of sparse dataframes with different sizes
@@ -67,7 +90,7 @@ print("\n\n###\n### Sparse DataFrame Experiments:\n###\n")
 for sdf in sparse_dataframes:
     print("Sparse Dataframe Size:",sdf.size)
     # Transfer data to DaphneLib
-    F = dc.from_pandas(sdf)
+    F = dc.from_pandas(sdf, verbose=True)
     # Appending and cartesian calculation
     F = F.rbind(F)
     F.compute()
@@ -76,7 +99,6 @@ for sdf in sparse_dataframes:
     print(F.compute())
 print("\n###End of Sparse DataFrame Experiments.\n")
 
-
 # MULTIINDEX DATAFRAME
 # Creating a list of multiindex dataframes with different sizes
 multiindex_dataframes = [pd.MultiIndex.from_product([list('"AB"'), range(size)]) for size in sizes]
@@ -84,7 +106,7 @@ multiindex_dataframes = [pd.MultiIndex.from_product([list('"AB"'), range(size)])
 print("\n\n###\n### MultiIndex Experiments:\n###\n")
 for midf in multiindex_dataframes:
     # Transfer data to DaphneLib
-    F = dc.from_pandas(midf)
+    F = dc.from_pandas(midf, verbose=True)
     # Appending and cartesian calculation
     F = F.rbind(F)
     F.compute()
@@ -101,7 +123,7 @@ print("\n\n###\n### Categorical Dataframe Experiments:\n###\n")
 for cdf in categorical_dataframes:
     cdf = cdf.astype("category")
     # Transfer data to DaphneLib
-    F = dc.from_pandas(cdf)
+    F = dc.from_pandas(cdf, verbose=True)
     # Appending and cartesian calculation
     F = F.rbind(F)
     F.compute()
@@ -109,3 +131,13 @@ for cdf in categorical_dataframes:
     F.compute()
     print(F.compute())
 print("\n###End of Categorical DataFrame Experiments.\n")
+
+#Create PerfoamnceResults Dataframe from Lists and export as CSV
+zipped = list(zip(type, dataframe_size, transfer_daphne_time, rbind_time, cartesian_time))
+PerfoamnceResults = pd.DataFrame(zipped, columns=['Type', 'dataframe_size', 'transfer_daphne_time', 'rbind_time', 'cartesian_time'])
+
+#PerfoamnceResults.to_csv("scripts/examples/daphnelib/results.csv")
+print(PerfoamnceResults)
+#print(type)
+#print(dataframe_size)
+# print(transfer_daphne_time)
