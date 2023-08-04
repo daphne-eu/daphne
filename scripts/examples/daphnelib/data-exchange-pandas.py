307e3694e248d680e81c311df379312c65d0b728
danielwetzel <daniel.wetzel@campus.tu-berlin.de>
2023-08-04 22:26:04 +0200

    Added Index Handling

--- scripts/examples/daphnelib/data-exchange-pandas.py
+++ scripts/examples/daphnelib/data-exchange-pandas.py
@@ -21,16 +21,37 @@ dc = DaphneContext()
 df = pd.DataFrame({"a": [1, 2, 3], "b": [1.1, -2.2, 3.3]})
 
 # Transfer data to DaphneLib
-F = dc.from_pandas(df, verbose=True)
+F = dc.from_pandas(df)
 
 #print(F)
 
 print("How DAPHNE sees the data from pandas:")
-F.print().compute(verbose=True)
+F.print().compute()
 
 # Append F to itself.
-F = F.rbind(F)
+F1 = F.rbind(F)
 
 # Compute in DAPHNE, transfer result back to Python.
 print("\nResult of appending the frame to itself, back in Python:")
-print(F.compute(verbose=True))
+print(F1.compute())
+
+
+print("\n\nShowcasing Indices in Daphne")
+# Create data in pandas.
+df2 = pd.DataFrame(
+    {"c": [30, 25, 0], 
+     "d": [ -1, 2.4, 5.9], 
+     "e": [7, 5, 9]})
+
+# Transfer data to DaphneLib with index
+F2 = dc.from_pandas(df2, keepIndex=True)
+
+print("How DAPHNE sees the data from pandas:")
+F2.print().compute(useIndexColumn=True)
+
+# Append F to F2 
+F2 = F2.cbind(F)
+
+# Compute in DAPHNE, transfer result back to Python.
+print("\nResult of appending both frames horizontally, back in Python:")
+print(F2.compute(useIndexColumn=True))
