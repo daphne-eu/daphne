Memory Overflow Fix

scripts/benchmarks/pandas-performancetest-3.py
@@ -28,7 +28,7 @@ import re
 runs = 10
 
 # Creating a list of sizes for the objects
-sizes = [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000]#, 5000000, 10000000, 50000000]
+sizes = [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000]#, 10000000, 50000000]
 cols = 25
 
 # Different Data Types for the benchmark
@@ -145,11 +145,11 @@ for run in range(runs):
 
         
         # Delete objects that are no longer need
-        del df
+        #del df
 
         # Delete the Object in Daphne to prevent Memory Overflow
         F.delete()
-        del F
+        #del F
 
         print(f'Progress: [{progress_bar}] {progress_percentage:.2f}% - Run {run + 1} - Total Size Processed: {total_size_gb:.3f} GB', end="\r")
 