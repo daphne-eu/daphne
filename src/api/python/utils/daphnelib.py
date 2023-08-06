66c955b125bcc8b718df4c768a27a9af9926f60b
danielwetzel <daniel.wetzel@campus.tu-berlin.de>
2023-08-06 19:58:47 +0200

    Memory Overflow Fix

--- src/api/python/utils/daphnelib.py
+++ src/api/python/utils/daphnelib.py
@@ -31,3 +31,6 @@ class DaphneLibResult(ctypes.Structure):
 
 DaphneLib = ctypes.CDLL(os.path.join(PROTOTYPE_PATH, "lib", "libdaphnelib.so"))
 DaphneLib.getResult.restype = DaphneLibResult
+
+DaphneLib.freeDaphneMemory.argtypes = [ctypes.POINTER(DaphneLibResult)]
+DaphneLib.freeDaphneMemory.restype = None
