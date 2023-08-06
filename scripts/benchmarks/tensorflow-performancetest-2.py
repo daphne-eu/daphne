316888cf5b7042978343b690e22c4b8369a801e7
danielwetzel <daniel.wetzel@campus.tu-berlin.de>
2023-08-06 02:27:16 +0200

    PyTorch & TensorFlow fixes

--- scripts/benchmarks/tensorflow-performancetest-2.py
+++ scripts/benchmarks/tensorflow-performancetest-2.py
@@ -34,7 +34,7 @@ dc = DaphneContext()
 
 text_stream = io.StringIO()
 
-header = ['num_dim1', 'num_dim2', 'num_dim3', 'exec_func_exec_time', 'tensor_trans_exec_time', 'overall_comp_exec_time']
+header = ['num_dim1', 'num_dim2', 'num_dim3', 'exec_func_exec_time', 'tensor_trans_time', 'overall_comp_exec_time', "tensor_size_mb"]
 testData = []
 
 # Set the iterations for the progress bar
@@ -44,6 +44,7 @@ current_iteration = 0
 # Set the iterations for the progress bar
 total_iterations = len(sizes) * runs
 current_iteration = 0
+total_size_gb = 0.0
 
 print("\n\n###\n### Tensorflow Tensor Compute Function Performance Test\n###\n")
 
@@ -52,7 +53,7 @@ print("\n\n###\n### Tensorflow Tensor Compute Function Performance Test\n###\n")
 for idx, run in enumerate(range(runs)): 
     for size in sizes:
         # Create a 3DTensor with the given size
-        tensor3d = tf.constant(np.random.randn(size,size,size))
+        tensor3d = tf.random.normal(shape=(size, size, size))
         #print(tensor3d)
         # Capture Verbose Outputs
         with contextlib.redirect_stdout(text_stream):
@@ -61,8 +62,13 @@ for idx, run in enumerate(range(runs)):
             print(f"Tensor length of Dim3:{tensor3d.get_shape()[2]}\n")
 
             # Transfer Tensor to DaphneLib
-            F = dc.from_tensorflow(tensor3d)
-            tensor3d = F.max(F).compute(isTensorflow=True, verbose=True)
+            T, orig_shape = dc.from_tensorflow(tensor3d, return_shape=True)
+            tensor3d = T.max(T).compute(isTensorflow=True, verbose=True, shape=orig_shape)
+
+        # Calculate the sizes for the tensor in MB and GB
+        tensor_size_bytes = tensor3d.numpy().size * 8  # assuming float64 data type
+        tensor_size_mb = tensor_size_bytes / (1024 ** 2)
+        total_size_gb += tensor_size_mb / 1024
 
         # Reset to the beginning of the text stream
         text_stream.seek(0)
@@ -94,7 +100,7 @@ for idx, run in enumerate(range(runs)):
         overall_comp_exec_time_match = re.search(r'Overall Compute Function execution time:\s+([\d.]+) seconds', captured_output)
         overall_comp_exec_time = float(overall_comp_exec_time_match.group(1)) if overall_comp_exec_time_match else None
 
-        
+        """
         # Debugging: Print the extracted values
         print(f'num_dim1: {num_dim1}')
         print(f'num_dim2: {num_dim2}')
@@ -104,10 +110,10 @@ for idx, run in enumerate(range(runs)):
         print(f'overall_comp_exec_time: {overall_comp_exec_time}')
 
         print(captured_output)
-        
+        """
 
         # Put the data together into a List
-        data = [num_dim1, num_dim2, num_dim3, exec_func_exec_time, tensor_trans_exec_time, overall_comp_exec_time]
+        data = [num_dim1, num_dim2, num_dim3, exec_func_exec_time, tensor_trans_exec_time, overall_comp_exec_time, tensor_size_mb]
         testData.append(data)
         
         # Clear the text stream for the next iteration
@@ -117,7 +123,7 @@ for idx, run in enumerate(range(runs)):
         # Update the progress bar
         current_iteration += 1
         progress_percentage = (current_iteration / total_iterations) * 100
-        bar_length = 40
+        bar_length = 30
         completed_length = int(bar_length * current_iteration // total_iterations)
         progress_bar = "#" * completed_length + "-" * (bar_length - completed_length)
 
@@ -126,10 +132,10 @@ for idx, run in enumerate(range(runs)):
         del tensor3d
 
         # Delete the Object in Daphne to prevent Memory Overflow
-        F.delete()
-        del F
+        T.delete()
+        del T
 
-        print(f'Progress: [{progress_bar}] {progress_percentage:.2f}% - Run {run + 1}', end="\r")
+        print(f'Progress: [{progress_bar}] {progress_percentage:.2f}% - Run {run + 1} - Total Size Processed: {total_size_gb:.3f} GB', end="\r")
 
 print("Benchmark finished! [")
 print()
