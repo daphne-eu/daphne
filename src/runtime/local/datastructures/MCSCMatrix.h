0328fec3f076980dff87e90367882fd8a8536817
ReneEnjilian <enjilianrene@gmail.com>
2023-09-11 22:29:26 +0200

    added documentation for MCSC matrix

--- src/runtime/local/datastructures/MCSCMatrix.h
+++ src/runtime/local/datastructures/MCSCMatrix.h
@@ -29,9 +29,41 @@
  #include <cstddef>
  #include <cstring>
 
+ /**
+ * @brief A modified sparse matrix in Modified Compressed Sparse Column (MCSC) format.
+ *
+ * Unlike the traditional CSC, this matrix implementation is designed to handle dynamic
+ * modifications more efficiently. Instead of a single contiguous array for non-zero values
+ * and row indices, each column has its own arrays, allowing for more flexible and efficient
+ * insertions and deletions.
+ *
+ * The `values` array contains arrays of non-zero values for each column. For every non-zero
+ * value in a column, the corresponding `rowIdxs` array provides the row index. The size of
+ * each column's arrays can be different, and might be larger than the actual number of non-zero
+ * values in that column, to accommodate future insertions.
+ *
+ * Each instance of this class might represent a sub-matrix of another `MCSCMatrix`. Traversing
+ * through columns and their respective values can be done efficiently without the need for
+ * additional offset arrays.
+ */
+
 
  template<typename ValueType>
  class MCSCMatrix : public Matrix<ValueType>{
+
+  /**
+   * @brief Detailed description of the class variables in the MCSCMatrix class.
+   *
+   * @param numRows Inherited from the Matrix class, representing the number of rows in the matrix.
+   * @param numCols Inherited from the Matrix class, representing the number of columns in the matrix.
+   * @param numColsAllocated The number of columns for which memory has been allocated.
+   * @param isColAllocatedBefore A flag indicating whether a column has been allocated before.
+   * @param maxNumNonZeros The maximum number of non-zero values that the matrix can hold.
+   * @param values A shared pointer to the array of arrays, holding the non-zero values of the matrix, organized by columns.
+   * @param rowIdxs A shared pointer to the array of arrays, holding the row indices corresponding to the non-zero values, organized by columns.
+   * @param valueSizes A shared pointer to the array indicating the number of non-zero values in each column.
+   * @param allocatedColSizes A shared pointer to the array indicating the allocated memory size for each column's arrays.
+   */
 
    using Matrix<ValueType>::numRows;
    using Matrix<ValueType>::numCols;
@@ -50,6 +82,20 @@
    template<class DataType>
    friend void DataObjectFactory::destroy(const DataType * obj);
 
+
+  /**
+   * @brief Constructor for the MCSCMatrix class.
+   *
+   * Initializes an MCSCMatrix with the specified number of rows, columns, and maximum non-zero values.
+   * Memory is allocated based on the `maxNumCols` and `maxNumNonZeros` parameters. Each column's
+   * arrays are initially allocated with a size that is slightly larger than the average column
+   * size (determined by `maxNumNonZeros` and `numCols`), to accommodate potential future insertions.
+   *
+   * @param numRows Number of rows in the matrix.
+   * @param maxNumCols Maximum number of columns the matrix can have.
+   * @param maxNumNonZeros Maximum number of non-zero values the matrix can hold.
+   * @param zero If set to true, initializes the matrix with all zero values; otherwise, the initial values are undefined.
+   */
 
    MCSCMatrix(size_t numRows, size_t maxNumCols, size_t maxNumNonZeros, bool zero) :
     Matrix<ValueType>(numRows, maxNumCols),
@@ -78,6 +124,19 @@
         }
     }
 
+  /**
+   * @brief View constructor for the MCSCMatrix class.
+   *
+   * Creates a new MCSCMatrix that represents a sub-matrix (specific columns) of the given `src` MCSCMatrix.
+   * The created matrix shares data with the source matrix to ensure efficient memory utilization. It allows
+   * the user to focus on a subset of columns without duplicating the underlying data.
+   *
+   * @param src Pointer to the source MCSCMatrix from which the sub-matrix view will be created.
+   * @param colLowerIncl The first column index (inclusive) in the source matrix to be included in the view.
+   * @param colUpperExcl The column index (exclusive) up to which columns are included in the view.
+   */
+
+
     MCSCMatrix(const MCSCMatrix<ValueType> * src, size_t colLowerIncl, size_t colUpperExcl) :
     Matrix<ValueType>(src->numRows, colUpperExcl - colLowerIncl),
     numColsAllocated(src->numColsAllocated - colLowerIncl),
@@ -95,24 +154,24 @@
     }
 
     void reallocateColumn(size_t colIdx) {
-        // 1. Determine the new size for the column.
+        // Determine the new size for the column
         const float growthFactor = 1.5;
         size_t currentSize = allocatedColSizes.get()[colIdx];
         size_t newSize = static_cast<size_t>(currentSize * growthFactor);
 
-        // 2. Allocate new arrays.
+        // Allocate new arrays
         std::shared_ptr<ValueType> newColumnValues(new ValueType[newSize], std::default_delete<ValueType[]>());
         std::shared_ptr<size_t> newColumnRowIdxs(new size_t[newSize], std::default_delete<size_t[]>());
 
-        // 3. Copy old data.
+        // Copy old data
         memcpy(newColumnValues.get(), values.get()[colIdx].get(), currentSize * sizeof(ValueType));
         memcpy(newColumnRowIdxs.get(), rowIdxs.get()[colIdx].get(), currentSize * sizeof(size_t));
 
-        // Initialize the rest of the new space with zeros.
+        // Initialize the rest of the new space with zeros
         memset(newColumnValues.get() + currentSize, 0, (newSize - currentSize) * sizeof(ValueType));
         memset(newColumnRowIdxs.get() + currentSize, 0, (newSize - currentSize) * sizeof(size_t));
 
-        // 4. Update the pointers.
+        // Update the pointers
         values.get()[colIdx] = newColumnValues;
         rowIdxs.get()[colIdx] = newColumnRowIdxs;
 
@@ -177,7 +236,6 @@ public:
 
 
   ValueType get(size_t rowIdx, size_t colIdx) const override {
-    // Check if column index is valid
     assert(colIdx < numCols && "colIdx is out of bounds");
 
     // Get the rows and values for the specified column
@@ -190,10 +248,9 @@ public:
     // Search for the row index in the current column's rowIdxs
     for(size_t i = 0; i < length; i++) {
         if (currentRowIdxs[i] == rowIdx) {
-            return currentValues[i];  // Return the value if found
+            return currentValues[i];
         }
     }
-
     // If row index wasn't found, the value is zero
     return static_cast<ValueType>(0);
   }
@@ -201,23 +258,23 @@ public:
   void set(size_t rowIdx, size_t colIdx, ValueType value) override {
     assert(rowIdx < numRows && colIdx < numCols && "Indices out of bounds");
 
-    // Retrieve the column's arrays and its current size.
+    // Retrieve the column's arrays and its current size
     ValueType* columnValues = values.get()[colIdx].get();
     size_t* columnRowIdxs = rowIdxs.get()[colIdx].get();
     size_t columnSize = valueSizes.get()[colIdx];
 
-    // If the value is zero and previously was non-zero, remove it.
+    // If the value is zero and previously was non-zero, remove it
     if (value == 0) {
         bool found = false;
         for (size_t i = 0; i < columnSize; i++) {
             if (columnRowIdxs[i] == rowIdx) {
                 found = true;
-                // Shift the remaining values and row indices.
+                // Shift the remaining values and row indices
                 for (size_t j = i; j < columnSize - 1; j++) {
                     columnValues[j] = columnValues[j + 1];
                     columnRowIdxs[j] = columnRowIdxs[j + 1];
                 }
-                // Reset the last (non-active) value and row index.
+                // Reset the last value and row index
                 columnValues[columnSize - 1] = 0;
                 columnRowIdxs[columnSize - 1] = 0;
 
@@ -241,16 +298,14 @@ public:
         }
 
         if (!updated) {
-            // The value is new, ensure we have space to add it.
+            // The value is new, ensure we have space to add it
             if (columnSize >= allocatedColSizes.get()[colIdx]) {
-                // Reallocation logic...
-                // (Increase the allocated space and copy the old values)
                 reallocateColumn(colIdx);
-                // Also adjust the column pointers after reallocation.
+                // Also adjust the column pointers after reallocation
                 columnValues = values.get()[colIdx].get();
                 columnRowIdxs = rowIdxs.get()[colIdx].get();
             }
-            // Append the new value and row index at the end.
+            // Append the new value and row index at the end
             columnValues[columnSize] = value;
             columnRowIdxs[columnSize] = rowIdx;
 
@@ -274,33 +329,31 @@ public:
     size_t* columnRowIdxs = rowIdxs.get()[colIdx].get();
     size_t columnSize = valueSizes.get()[colIdx];
 
-    // If we've used up all the allocated space, reallocate more memory.
+    // If we've used up all the allocated space, reallocate more memory
     if (columnSize >= allocatedColSizes.get()[colIdx]) {
-        // Reallocation logic...
         reallocateColumn(colIdx);
-
         // Also adjust the column pointers after reallocation.
         columnValues = values.get()[colIdx].get();
         columnRowIdxs = rowIdxs.get()[colIdx].get();
     }
 
-    // Find the right position to insert the new value.
+    // Find the right position to insert the new value
     size_t position = 0;
     while (position < columnSize && columnRowIdxs[position] < rowIdx) {
         position++;
     }
 
-    // Shift the values and row indices to the right from the found position.
+    // Shift the values and row indices to the right from the found position
     for (size_t i = columnSize; i > position; i--) {
         columnValues[i] = columnValues[i - 1];
         columnRowIdxs[i] = columnRowIdxs[i - 1];
     }
 
-    // Insert the new value and its row index.
+    // Insert the new value and its row index
     columnValues[position] = value;
     columnRowIdxs[position] = rowIdx;
 
-    // Increase the size for this column.
+    // Increase the size for this column
     valueSizes.get()[colIdx]++;
   }
 
@@ -369,7 +422,7 @@ public:
   }
 
   size_t serialize(std::vector<char> &buf) const override{
-    // MCSRMatrix is not yet integrated into DaphneSerializer
+    // MCSCMatrix is not yet integrated into DaphneSerializer
     return 0;
   }
 
