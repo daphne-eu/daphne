5d83b7496886af1bb6a0fabd77a75c2b23f41998
Mark Dokter <mark@dokter.cc>
2023-11-06 15:47:31 +0100

    spoof matrix rename -> CCMatrix due to name clash with DAPHNE's Matrix class

--- src/compiler/codegen/spoof-launcher/rowbins.h
+++ src/compiler/codegen/spoof-launcher/rowbins.h
@@ -4,4 +4,4 @@
 #include "Matrix.h"
 
 template<class T>
-void rowBins(Matrix<T>* mat, uint32_t* bin_sizes, uint32_t* bins, const uint32_t* row_ptrs, size_t num_rows);
+void rowBins(CCMatrix<T>* mat, uint32_t* bin_sizes, uint32_t* bins, const uint32_t* row_ptrs, size_t num_rows);
