77561999525867d66bd89664e6e19442b7d1928d
Mark Dokter <mark@dokter.cc>
2023-11-22 17:29:48 +0100

    fix CastObjTest.cpp

--- test/runtime/local/kernels/CastObjTest.cpp
+++ test/runtime/local/kernels/CastObjTest.cpp
@@ -19,8 +19,7 @@
 #include <runtime/local/datastructures/DenseMatrix.h>
 #include <runtime/local/datastructures/Frame.h>
 #include <runtime/local/datastructures/Structure.h>
-#include <runtime/local/datastructures/ValueTypeCode.h>
-#include <SIMDOperators/datastructure/column.hpp>
+#include <SIMDOperators/datastructures/column.hpp>
 #include <runtime/local/kernels/CastObj.h>
 #include <runtime/local/kernels/CheckEq.h>
 
@@ -29,8 +28,6 @@
 #include <catch.hpp>
 
 #include <vector>
-
-#include <cstdint>
 
 TEMPLATE_PRODUCT_TEST_CASE("castObj, frame to matrix, single-column", TAG_KERNELS, (DenseMatrix), (double, int64_t, uint32_t)) {
     using DTRes = TestType;
