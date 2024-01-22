8927e7e515ce8f2a8ae004bd337bd20140c3ff0e
m-birke <wesci@web.de>
2024-01-22 13:03:20 +0100

    adopt import path of dlib tests

--- test/api/python/matrix_outerbinary.py
+++ test/api/python/matrix_outerbinary.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from api.python.context.daphne_context import DaphneContext
+from api.python.daphne.context.daphne_context import DaphneContext
 
 dc = DaphneContext()
 
