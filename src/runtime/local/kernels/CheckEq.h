456ae114ddf6e843505851dce0fefe5fc150acb6
CPestka <constantin.pestka@c-pestka.de>
2024-04-17 18:20:59 +0200

    removed unnecessary includes

--- src/runtime/local/kernels/CheckEq.h
+++ src/runtime/local/kernels/CheckEq.h
@@ -24,6 +24,7 @@
 #include <runtime/local/datastructures/ChunkedTensor.h>
 #include <runtime/local/datastructures/Matrix.h>
 
+
 // ****************************************************************************
 // Struct for partial template specialization
 // ****************************************************************************
