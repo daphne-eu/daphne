65762d3abd4087edec25f51c0fdd69ce2a70b5e2
Mark Dokter <mark@dokter.cc>
2024-05-14 16:52:45 +0200

    [DAPHNE-#xyz] ReLU backward pass
    This commit adds the necessary code changes to call CUDNN's activation backward function with ReLU activation type. No tests yet.

--- src/runtime/local/kernels/CUDA/Activation.cpp
+++ src/runtime/local/kernels/CUDA/Activation.cpp
@@ -44,7 +44,42 @@ namespace CUDA::NN::Activation {
                 d_input, &blend_beta, ctx->dst_tensor_desc, d_res));
     }
 
+    template<typename OP, typename DTRes, typename DTArg>
+    void Backward<OP, DTRes, DTArg>::apply(DTRes *&res, const DTArg *grad_in, const DTArg *data_in, DCTX(dctx)) {
+        const size_t deviceID = 0; //ToDo: multi device support
+        auto ctx = CUDAContext::get(dctx, deviceID);
+        AllocationDescriptorCUDA alloc_desc(dctx, deviceID);
+        using VT = typename DTRes::VT;
+        const size_t nr1 = data_in->getNumRows();
+        const size_t nc1 = data_in->getNumCols();
+        const VT blend_alpha = 1;
+        const VT blend_beta = 0;
+        const VT* d_grad_in = grad_in->getValues(&alloc_desc);
+        const VT* d_data_in = data_in->getValues(&alloc_desc);
+
+        if (res == nullptr) {
+            res = DataObjectFactory::create<DTRes>(nr1, nc1, false, &alloc_desc);
+        }
+        VT* d_res = res->getValues(&alloc_desc);
+
+        CHECK_CUDNN(cudnnSetTensor4dDescriptor(ctx->src_tensor_desc, ctx->tensor_format, ctx->getCUDNNDataType<VT>(), 1,
+                1, grad_in->getNumRows(), grad_in->getNumCols()));
+        CHECK_CUDNN(cudnnSetTensor4dDescriptor(ctx->src2_tensor_desc, ctx->tensor_format, ctx->getCUDNNDataType<VT>(), 1, 1, nr1, nc1));
+        CHECK_CUDNN(cudnnSetTensor4dDescriptor(ctx->dst_tensor_desc, ctx->tensor_format, ctx->getCUDNNDataType<VT>(), 1, 1, nr1, nc1));
+
+        CHECK_CUDNN(cudnnSetActivationDescriptor(ctx->activation_desc, OP::getActivationType(), CUDNN_PROPAGATE_NAN, 0.0));
+
+        CHECK_CUDNN(cudnnActivationBackward(ctx->getCUDNNHandle(), ctx->activation_desc, &blend_alpha,
+                ctx->src_tensor_desc, d_grad_in, ctx->src_tensor_desc, d_grad_in, ctx->src2_tensor_desc, d_data_in,
+                &blend_beta, ctx->dst_tensor_desc, d_res));
+    }
+
+
+
     template struct Forward<ReLU, DenseMatrix<float>, DenseMatrix<float>>;
     template struct Forward<ReLU, DenseMatrix<double>, DenseMatrix<double>>;
+
+    template struct Backward<ReLU, DenseMatrix<float>, DenseMatrix<float>>;
+    template struct Backward<ReLU, DenseMatrix<double>, DenseMatrix<double>>;
 }
 
