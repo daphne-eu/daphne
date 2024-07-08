fe41fefe084ac648e2fd65fa0faef4c37cf8bbf5
WangYuyao <924953347@qq.com>
2024-07-08 07:21:04 +0200

    AMLS Exercise

--- test/runtime/local/kernels/DNNConv2DBackwardDataTest.cpp
+++ test/runtime/local/kernels/DNNConv2DBackwardDataTest.cpp
@@ -18,42 +18,6 @@
 
 #ifdef USE_CUDA
 
-#include <runtime/local/datagen/GenGivenVals.h>
-#include <runtime/local/datastructures/DenseMatrix.h>
-#include <runtime/local/kernels/CheckEq.h>
-
-#include <cassert>
-#include <tags.h>
-#include <catch.hpp>
-
-#include "runtime/local/kernels/CUDA/Convolution.h"
-
-template<class DT>
-void check(const DT* in, const DT* filter, const DT* exp, DaphneContext* dctx) {
-    DT* res = nullptr;
-    size_t out_h;
-    size_t out_w;
-    CUDA::Convolution::Forward<DT, DT>::apply(res, out_h, out_w, in, filter, nullptr, in->getNumRows(), 1, 3, 3, 2, 2,
-            1, 1, 0, 0, dctx);
-    CHECK(*res == *exp);
-}
-
-TEMPLATE_PRODUCT_TEST_CASE("conv_fwd", TAG_DNN, (DenseMatrix), (float, double)) { // NOLINT(cert-err58-cpp)
-    auto dctx = setupContextAndLogger();
-    using DT = TestType;
-
-    auto input = genGivenVals<DT>(1, { 1, 2, 3, 4, 5, 6, 7, 8, 9});
-    auto filter = genGivenVals<DT>(1, { 1, 0, 0, 1});
-
-    // expected output when used with settings filter 2x2, stride 1x1, padding 0x0
-    auto result = genGivenVals<DT>(1, { 6, 8, 12, 14 });
-
-    check(input, filter, result, dctx.get());
-
-    DataObjectFactory::destroy(input);
-    DataObjectFactory::destroy(result);
-}
-
 #endif // USE_CUDA
 
 #include <runtime/local/datagen/GenGivenVals.h>
