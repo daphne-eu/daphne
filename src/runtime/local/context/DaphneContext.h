2211a32cfaa54e099d2ff2fada4ecbac7ddad828
Mark Dokter <mark@dokter.cc>
2024-07-19 15:47:51 +0200

    [DAPHNE-#788] Tracking string allocations to close memory leaks
    This commit extends the ManageObjectRefs compiler pass and its runtime kernels to track usage of dynamically allocated string data and delete it after its last use. So far this has been found necessary with string concatenation and casting operations. Strings allocated by the compiler as constants will have their reference counter increased upon creation, which should prevent them from being garbage collected. For a detailed description see issue #788 and discussion in pull request #791.
    
    Fixes #788, Closes #791
    
    Co-authored-by: Patrick Damme <patrick.damme@tu-berlin.de>

--- src/runtime/local/context/DaphneContext.h
+++ src/runtime/local/context/DaphneContext.h
@@ -19,6 +19,7 @@
 #include <api/cli/DaphneUserConfig.h>
 #include <util/KernelDispatchMapping.h>
 #include <util/Statistics.h>
+#include <util/StringRefCount.h>
 
 #include <vector>
 #include <iostream>
@@ -58,7 +59,6 @@ struct DaphneContext {
     std::vector<std::unique_ptr<IContext>> cuda_contexts;
     std::vector<std::unique_ptr<IContext>> fpga_contexts;
 
-
     std::unique_ptr<IContext> distributed_context;
 
     /**
@@ -71,13 +71,15 @@ struct DaphneContext {
     DaphneUserConfig& config;
     KernelDispatchMapping& dispatchMapping;
     Statistics& stats;
+    StringRefCounter& stringRefCount;
 
     std::shared_ptr<spdlog::logger> logger;
 
     explicit DaphneContext(DaphneUserConfig &config,
                            KernelDispatchMapping &dispatchMapping,
-                           Statistics &stats)
-        : config(config), dispatchMapping(dispatchMapping), stats(stats) {
+                           Statistics &stats,
+                           StringRefCounter& stringRefCnt)
+        : config(config), dispatchMapping(dispatchMapping), stats(stats), stringRefCount(stringRefCnt) {
         logger = spdlog::get("runtime");
     }
 
@@ -90,8 +92,6 @@ struct DaphneContext {
         }
         cuda_contexts.clear();
         fpga_contexts.clear();
-
-
     }
 
 #ifdef USE_CUDA
@@ -124,4 +124,6 @@ struct DaphneContext {
     }
 
     [[nodiscard]] DaphneUserConfig &getUserConfig() const { return config; }
+
+
 };
