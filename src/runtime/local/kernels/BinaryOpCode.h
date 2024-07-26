333b752bbc904346c14c2800f7d14174b3f68262
Patrick Damme <patrick.damme@tu-berlin.de>
2024-07-26 22:20:08 +0200

    Guarded instantiation of unary/binary ops.
    - Motivation:
      - DAPHNE supports various elementwise unary/binary operations as well as various value types.
      - Not all combinations of unary/binary operations and argument/result value types are meaningful.
        - For instance, one cannot calculate the square root of a string.
      - So far, this was not a severe problem, since we have mostly numeric value types and meaningful operations thereon.
      - However, we already had to introduce workarounds for equality and concat on strings.
        - More precisely, we use StringEqOp instead of EwEqOp and ConcatOp instead of EwConcatOp for strings, which is inconsistent with the rest of DaphneIR.
      - In the future, this problem will get amplified in the context of extensibility for value types, where we want to be able to add custom value types and allow them to reuse the existing elementwise unary/binary kernels, but only for some ops.
    - Technical problem:
      - The functions getEwUnaryScaFuncPtr()/getEwBinaryScaFuncPtr() are used in the elementwise unary/binary kernels.
      - These functions get the op code as a runtime parameter, switch-case on it, and in each case, instantiate the kernel with the op code as a template parameter.
      - However, this implies that these functions can only be instantiated with value types that support *all* unary/binary operations.
    - Solution:
      - Introduced C++ compile-time information specifying which unary/binary operations should be supported on which combinations of argument/result value types.
      - Technically, this is achieved through the boolean template variables supportsUnaryOp/supportBinaryOp, which reside in UnaryOpCode.h/BinaryOpCode.h.
      - These variables are used to guard the kernel instantiations in getEwUnaryScaFuncPtr()/getEwBinaryScaFuncPtr() through if-constexpr.
      - Note that this mechanism is orthogonal/complementary to the specifications in kernels.json.
    - Additional contributions:
      - Restructuring and harmonization of UnaryOpCode.h and BinaryOpCode.h
        - for each of them we have (replace X by un/bin)
          - enum XaryOpCode
          - std::string_view Xary_op_codes[]
          - template<..., XaryOpCode op> static constexpr bool supportsXaryOp = false;
          - helper macros for concisely specifying which ops should be supported on which value types
      - Improved some related error messages.
    - So far, this is only a refactoring, a follow-up commit will make use of the changes to get rid of the workarounds for StringEqOp and ConcatOp, as one concrete example of the usefulness.

--- src/runtime/local/kernels/BinaryOpCode.h
+++ src/runtime/local/kernels/BinaryOpCode.h
@@ -16,6 +16,10 @@
 
 #pragma once
 
+// ****************************************************************************
+// Enum for binary op codes and their names
+// ****************************************************************************
+
 enum class BinaryOpCode {
     // Arithmetic.
     ADD,  // addition
@@ -25,7 +29,6 @@ enum class BinaryOpCode {
     POW,  // to the power of
     MOD,  // modulus
     LOG,  // logarithm
-
     // Comparisons.
     EQ,   // equal
     NEQ,  // not equal
@@ -33,18 +36,124 @@ enum class BinaryOpCode {
     LE,   // less equal
     GT,   // greater than
     GE,   // greater equal
-
     // Min/max.
     MIN,
     MAX,
-
     // Logical.
     AND,
     OR,
-
     // Bitwise.
     BITWISE_AND,
 };
 
-static std::string_view binary_op_codes[] = {"ADD", "SUB", "MUL", "DIV", "POW", "MOD", "LOG", "EQ", "NEQ", "LT", "LE",
-        "GT", "GE", "MIN", "MAX", "AND", "OR", "BITWISE_AND"};
+/**
+ * @brief Array of the "names" of the `BinaryOpCode`s.
+ *
+ * Must contain the same elements as `BinaryOpCode` in the same order,
+ * such that we can obtain the name corresponding to a `BinaryOpCode` `opCode`
+ * by `binary_op_codes[static_cast<int>(opCode)]`.
+ */
+static std::string_view binary_op_codes[] = {
+    // Arithmetic.
+    "ADD", "SUB", "MUL", "DIV", "POW", "MOD", "LOG",
+    // Comparisons.
+    "EQ", "NEQ", "LT", "LE", "GT", "GE",
+    // Min/max.
+    "MIN", "MAX",
+    // Logical.
+    "AND", "OR",
+    // Bitwise.
+    "BITWISE_AND"
+};
+
+// ****************************************************************************
+// Specification which binary ops should be supported on which value types
+// ****************************************************************************
+
+/**
+ * @brief Template constant specifying if the given binary operation
+ * should be supported on arguments of the given value types.
+ *
+ * @tparam VTRes The result value type.
+ * @tparam VTLhs The left-hand-side argument value type.
+ * @tparam VTRhs The right-hand-side argument value type.
+ * @tparam op The binary operation.
+ */
+template<BinaryOpCode op, typename VTRes, typename VTLhs, typename VTRhs>
+static constexpr bool supportsBinaryOp = false;
+
+// Macros for concisely specifying which binary operations should be
+// supported on which value types.
+
+// Generates code specifying that the binary operation `Op` should be supported on
+// the value type `VT` (for the result and the two arguments, for simplicity).
+#define SUPPORT(Op, VT) \
+    template<> constexpr bool supportsBinaryOp<BinaryOpCode::Op, VT, VT, VT> = true;
+
+// Generates code specifying that all binary operations of a certain category should be
+// supported on the given value type `VT` (for the result and the two arguments, for simplicity).
+#define SUPPORT_ARITHMETIC(VT) \
+    /* Arithmetic. */ \
+    SUPPORT(ADD, VT) \
+    SUPPORT(SUB, VT) \
+    SUPPORT(MUL, VT) \
+    SUPPORT(DIV, VT) \
+    SUPPORT(POW, VT) \
+    SUPPORT(MOD, VT) \
+    SUPPORT(LOG, VT)
+#define SUPPORT_EQUALITY(VT) \
+    /* Comparisons. */ \
+    SUPPORT(EQ , VT) \
+    SUPPORT(NEQ, VT)
+#define SUPPORT_COMPARISONS(VT) \
+    /* Comparisons. */ \
+    SUPPORT(LT, VT) \
+    SUPPORT(LE, VT) \
+    SUPPORT(GT, VT) \
+    SUPPORT(GE, VT) \
+    /* Min/max. */ \
+    SUPPORT(MIN, VT) \
+    SUPPORT(MAX, VT)
+#define SUPPORT_LOGICAL(VT) \
+    /* Logical. */ \
+    SUPPORT(AND, VT) \
+    SUPPORT(OR , VT)
+#define SUPPORT_BITWISE(VT) \
+    /* Bitwise. */ \
+    SUPPORT(BITWISE_AND, VT)
+
+// Generates code specifying that all binary operations typically supported on a certain
+// category of value types should be supported on the given value type `VT`
+// (for the result and the two arguments, for simplicity).
+#define SUPPORT_NUMERIC_FP(VT) \
+    SUPPORT_ARITHMETIC(VT) \
+    SUPPORT_EQUALITY(VT) \
+    SUPPORT_COMPARISONS(VT) \
+    SUPPORT_LOGICAL(VT)
+#define SUPPORT_NUMERIC_INT(VT) \
+    SUPPORT_ARITHMETIC(VT) \
+    SUPPORT_EQUALITY(VT) \
+    SUPPORT_COMPARISONS(VT) \
+    SUPPORT_LOGICAL(VT) \
+    SUPPORT_BITWISE(VT)
+
+// Concise specification of which binary operations should be supported on
+// which value types.
+SUPPORT_NUMERIC_FP(double)
+SUPPORT_NUMERIC_FP(float)
+SUPPORT_NUMERIC_INT(int64_t)
+SUPPORT_NUMERIC_INT(int32_t)
+SUPPORT_NUMERIC_INT(int8_t)
+SUPPORT_NUMERIC_INT(uint64_t)
+SUPPORT_NUMERIC_INT(uint32_t)
+SUPPORT_NUMERIC_INT(uint8_t)
+
+// Undefine helper macros.
+#undef SUPPORT
+#undef SUPPORT_ARITHMETIC
+#undef SUPPORT_EQUALITY
+#undef SUPPORT_COMPARISONS
+#undef SUPPORT_LOGICAL
+#undef SUPPORT_BITWISE
+#undef SUPPORT_NUMERIC_FP
+#undef SUPPORT_NUMERIC_INT
