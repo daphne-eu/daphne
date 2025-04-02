f8e3a05dfe0a25463b6b8e679dee73f3eff73781
Christian Schwarz <cmrs@cmrs.io>
2025-04-02 04:14:07 +0000

    remove instrumentation from destroyDaphneContext

--- src/runtime/local/kernels/genKernelInst.py
+++ src/runtime/local/kernels/genKernelInst.py
@@ -154,8 +154,9 @@ def generateKernelInstantiation(kernelTemplateInfo, templateValues, opCodes, out
             rp["isOutput"] = False
 
     isCreateDaphneContext = opName == "createDaphneContext"
+    isDestroyDaphneContext = opName == "destroyDaphneContext"
     def isInstrumentedOp(op :str):
-        if op in ["map", "createDaphneContext"]:
+        if op in ["map", "createDaphneContext", "destroyDaphneContext"]:
             return False
         # TODO: KernelDispatchMapping currently does not support distributed ops
         if "distributed" in op:
@@ -179,8 +180,9 @@ def generateKernelInstantiation(kernelTemplateInfo, templateValues, opCodes, out
     if typesForName != "":
         typesForName = "__" + typesForName
     params = ", ".join(
-        ["{} {}".format(rtp["type"], rtp["name"]) for rtp in
-         extendedRuntimeParams] + ([] if not isInstrumented else ["int kId"]) + ([] if isCreateDaphneContext else ["DCTX(ctx)"])
+        ["{} {}".format(rtp["type"], rtp["name"]) for rtp in extendedRuntimeParams]
+        + ([] if (not isInstrumented and not isDestroyDaphneContext) else ["int kId"])
+        + ([] if isCreateDaphneContext else ["DCTX(ctx)"])
     )
 
     def generateFunction(opCode):
