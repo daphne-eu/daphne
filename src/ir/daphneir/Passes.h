a39a8a4e3a50f2cb4c770501238b04152d4847a4
Daniil <d.ossinkin@gmail.com>
2025-07-11 19:09:34 +0000

    some attempt to get reduction into parfor

--- src/ir/daphneir/Passes.h
+++ src/ir/daphneir/Passes.h
@@ -72,7 +72,7 @@ std::unique_ptr<Pass> createSelectMatrixRepresentationsPass(const DaphneUserConf
 std::unique_ptr<Pass> createSpecializeGenericFunctionsPass(const DaphneUserConfig &cfg);
 std::unique_ptr<Pass> createTransposeOpLoweringPass();
 std::unique_ptr<Pass> createVectorizeComputationsPass();
-std::unique_ptr<Pass> createParForOpLoweringPass();
+std::unique_ptr<Pass> createParForReductionDetectionPass();
 
 #ifdef USE_CUDA
 std::unique_ptr<Pass> createMarkCUDAOpsPass(const DaphneUserConfig &cfg);
